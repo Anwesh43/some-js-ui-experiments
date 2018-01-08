d1ae176ea3bf4fe880d35d2d9c522bc073d65c33
anwesh43 <anweshthecool0@gmail.com>
2018-01-08 11:09:21 +0530

    adding Mover class

--- TouchDownBallMover.js
+++ TouchDownBallMover.js
@@ -1,3 +1,4 @@
+const w = window.innerWidth, h = window.innerHeight,size = Math.min(w,h)/20
 class Point {
     constructor(x,y) {
         this.x = x
@@ -14,5 +15,25 @@ class Point {
             this.prevX = x
             this.prevY = y
         }
+    }
+}
+class Mover {
+    constructor() {
+        this.point = new Point(w/2,h/2)
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.point.x,this.point.y)
+        context.fillStyle = 'yellowgreen'
+        context.beginPath()
+        context.arc(0,0,size/2,0,2*Math.PI)
+        context.fill()
+        context.restore()
+    }
+    update(stopcb) {
+
+    }
+    startUpdating(startcb) {
+
     }
 }
