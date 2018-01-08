609c467c1ca5843cfcf0e871d3bca2c6b69e6d30
anwesh43 <anweshthecool0@gmail.com>
2018-01-08 11:31:49 +0530

    adding State

--- TouchDownBallMover.js
+++ TouchDownBallMover.js
@@ -37,3 +37,24 @@ class Mover {
 
     }
 }
+class State {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir*0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.dir = 0
+            this.scale = this.prevScale+this.dir
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1-2*this.scale
+        }
+    }
+}
