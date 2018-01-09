c447a8245d99438cc9688f084242e01afd1daad8
anwesh43 <anweshthecool0@gmail.com>
2018-01-09 18:49:16 +0530

    adding DimensionController

--- DimensionController.js
+++ DimensionController.js
@@ -8,6 +8,8 @@ class DimensionController {
             this.w = window.innerWidth
             this.h = window.innerHeight
             update(this.w,this.h)
+            console.log(this.w)
+            console.log(this.h)
         }
     }
 }
