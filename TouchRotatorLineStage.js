d4bbe326701c745f7e65ddc850aa15f92ab6d45d
anwesh43 <anweshthecool0@gmail.com>
2018-01-09 15:29:12 +0530

    adding TouchRotatorLine

--- TouchRotatorLineStage.js
+++ TouchRotatorLineStage.js
@@ -1,3 +1,30 @@
+class TouchRotatorLine {
+    constructor(x,y,w) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.deg = 0
+    }
+    draw(context) {
+        context.save()
+        context.translate(this.x,this.y)
+        contexr.rotate(this.deg*Math.PI/180)
+        context.strokeStyle = 'yellowgreen'
+        context.lineWidth = this.w/30
+        context.lineCap = 'round'
+        context.beginPath()
+        context.moveTo(0,0)
+        context.lineTo(this.w,0)
+        context.stroke()
+        context.restore()
+    }
+    startUpdating(deg,startcb) {
+
+    }
+    update(stopcb) {
+
+    }
+}
 class TouchRotatorLineStage extends CanvasStage{
     constructor() {
         super()
