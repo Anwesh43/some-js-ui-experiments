3161080bc5daa394d3b2f7c41ca288a2e246c3fc
anwesh43 <anweshthecool0@gmail.com>
2018-01-09 13:00:24 +0530

    adding TouchRotatorLineStage

---
+++ TouchRotatorLineStage.js
@@ -0,0 +1,12 @@
+class TouchRotatorLineStage extends CanvasStage{
+    constructor() {
+        super()
+    }
+    render() {
+        super.render()
+    }
+}
+const initTouchRotatorStage = () => {
+    const stage = new TouchRotatorLineStage()
+    stage.render()
+}
