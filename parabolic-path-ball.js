0be248676ad786386c1f51d1430b7d15df643a0d
anwesh43 <anweshthecool0@gmail.com>
2018-01-10 10:30:30 +0530

    adding Point class

--- parabolic-path-ball.js
+++ parabolic-path-ball.js
@@ -14,15 +14,33 @@ class ParabolicPathStage extends CanvasStage{
 class ParabolicPath {
     constructor(x,y) {
         this.points = []
+        this.px = x
+        this.py = y
+        this.x = 0
+        this.y = 0
+        this.state = new State()
     }
-    draw(context) {
-
+    draw(context,r) {
+        context.save()
+        context.translate(this.x,this.y)
+        for(var i=0;i<2;i++) {
+            context.save()
+            context.scale(1-2*i,1)
+            context.beginPath()
+            context.arc(this.x,this.y,r,0,2*Math.PI)
+            context.fill()
+            context.restore()
+        }
+        context.restore()
     }
     update(stopcb) {
+        this.state.update(()=>{
+
+        },stopcb)
 
     }
     startUpdating(startcb) {
-
+        this.state.update(stopcb)
     }
 }
 class ParabolicState {
@@ -31,7 +49,7 @@ class ParabolicState {
         this.y = 0
         this.dir = 0
     }
-    update(updatecb) {
+    update(updatecb,stopcb) {
         this.x+=dir
         this.y = this.x*this.x
         if(this.x >= 10) {
@@ -39,6 +57,7 @@ class ParabolicState {
         }
         if(this.x < = 0) {
             this.dir = 0
+            stopcb()
         }
         updatecb(this.x,this.y)
     }
@@ -46,5 +65,14 @@ class ParabolicState {
         if(this.dir == 0) {
             this.dir = 1
         }
+    }
+}
+class Point {
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+    }
+    equals(x,y) {
+        return (this.x == x && this.y == y)
     }
 }
