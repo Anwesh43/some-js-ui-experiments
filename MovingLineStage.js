2d327a3aba3b9b73e243f07cc0f2838b1c2e3a33
anwesh43 <anweshthecool0@gmail.com>
2018-01-12 09:19:49 +0530

    adding MovingPointContainer

--- MovingLineStage.js
+++ MovingLineStage.js
@@ -23,11 +23,44 @@ class MovingPoint {
         return new MovingPoint(this.x,this.y,x,y)
     }
     drawFromPivot(context) {
-        context.strokeStyle = 'yellowgreen'
         context.lineWidth = 10
         context.beginPath()
         context.moveTo(this.ox,this.y)
         context.lineTo(this.x,this.y)
         context.stroke()
+        context.beginPath()
+        context.arc(this.ox,this.oy,20,0,2*Math.PI)
+        context.fill()
+    }
+}
+class MovingPointContainer {
+    constructor(size) {
+        this.points = []
+        this.start = size
+    }
+    draw(context) {
+        context.strokeStyle = 'yellowgreen'
+        context.fillStyle = 'yellowgreen'
+        this.points.forEach((point)=>{
+            point.drawFromPivot(context)
+        })
+        if(this.points.length == 0) {
+            context.beginPath()
+            context.arc(20,this.start.y,20,0,2*Math.PI)
+            context.fill()
+        }
+    }
+    addPoint(x,y,startcb,resetcb) {
+        if(this.points.length > 0) {
+            const prevPoint = this.points[this.points.length-1]
+            const newPoint = prevPoint.createFromMe(x,y)
+            this.points.push(newPoint)
+            resetcb()
+        }
+        else {
+            const newPoint = new MovingPoint(20,this.start.y/2,x,y)
+            this.points.push(newPoint)
+            startcb()
+        }
     }
 }
