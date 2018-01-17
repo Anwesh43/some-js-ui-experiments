6ba36b2b6d8b6c0e45c9f7c94218a19ca82165ac
anwesh43 <anweshthecool0@gmail.com>
2018-01-17 12:56:54 +0530

    adding LinkedPathAnimator

--- LinkedPathStage.js
+++ LinkedPathStage.js
@@ -114,3 +114,22 @@ const initLinkedPathStage = () => {
     const stage = new LinkedPathStage()
     stage.render()
 }
+class LinkedPathAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            })
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
