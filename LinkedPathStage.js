f319f003ec62408ed2554b2346ad50eb8995f37c
anwesh43 <anweshthecool0@gmail.com>
2018-01-17 11:20:15 +0530

    adding NodePoint

--- LinkedPathStage.js
+++ LinkedPathStage.js
@@ -6,3 +6,17 @@ class LinkedPathStage extends Stage {
         super.render();
     }
 }
+class NodePoint {
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+        this.prev = null
+        this.next = null
+    }
+    addPrevPoint(point) {
+        this.prev = point
+    }
+    addNextPoint(point) {
+        this.next = point
+    }
+}
