ec0c51791489a97a7151feb9de1e0e19c87a38ab
anwesh43 <anweshthecool0@gmail.com>
2018-01-17 12:13:29 +0530

    adding LinkedPath

--- LinkedPathStage.js
+++ LinkedPathStage.js
@@ -20,3 +20,54 @@ class NodePoint {
         this.next = point
     }
 }
+class LinkedPath {
+    constructor(x,y,w,h) {
+        this.mainPoint = new NodePoint(x,y)
+        this.initPath(x,y,w,h)
+    }
+    initPath(x,y,w,h) {
+        var root = this.mainPoint
+        const x_pos_node = this.iterateTill(x,x+w,1,root,y,0)
+        const y_pos_node = this.iterateTill(y,y+h,1,x_pos_node,x+w,1)
+        const x_neg_node = this.iterateTill(x+w,x,-1,y_pos_node,y+h,0)
+        const y_neg_node = this.iterateTill(y+h,y,-1,x_neg_node,x,1)
+        y_neg_node.addNextPoint(root)
+        root.addPrevPoint(y_neg_node)
+    }
+    iterateTill(start,end,dir,root,fixed,mode) {
+        var node = root
+        var i = start
+        while(true) {
+            i += dir
+            var curr = new NodePoint(i,fixed)
+            if(mode == 1) {
+                curr = new NodePoint(fixed,i)
+            }
+            node.addNextPoint(curr)
+            curr.addPrevPoint(node)
+            node = curr
+            if(i == end) {
+                break
+            }
+        }
+        return node
+    }
+    drawThePath(context) {
+        context.strokeStyle = '#7f8c8d'
+        context.beginPath()
+        context.moveTo(this.movePoint.x,this.movePoint.y)
+        this.iterateThroughPoints(context)
+    }
+    iterateThroughPoints(context) {
+        var node = this.movePoint
+        while (true) {
+            var curr = node.next
+            if(curr == this.movePoint) {
+                context.stroke()
+                break
+            }
+            context.lineTo(curr.x,curr.y)
+            node = curr
+        }
+    }
+}
