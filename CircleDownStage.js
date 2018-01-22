3fe9e6e37eb7a1549abfb3c663649d7eef462454
anwesh43 <anweshthecool0@gmail.com>
2018-01-22 10:55:54 +0530

    adding CircleDownState

--- CircleDownStage.js
+++ CircleDownStage.js
@@ -42,6 +42,28 @@ class CircleDown {
 
     }
     startUpdating(startcb) {
-        
+
+    }
+}
+class CircleDownState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scale += 0.1*this.dir
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.dir = 0
+            this.prevScale = 1
+            this.scale = 1
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1-2*this.scale
+            startcb()
+        }
     }
 }
