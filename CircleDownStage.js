e730fe5e4e4e87cfb71314ce018bc550099a32af
anwesh43 <anweshthecool0@gmail.com>
2018-01-22 11:11:23 +0530

    adding Animator

--- CircleDownStage.js
+++ CircleDownStage.js
@@ -68,3 +68,22 @@ class CircleDownState {
         }
     }
 }
+class CircleDownAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
