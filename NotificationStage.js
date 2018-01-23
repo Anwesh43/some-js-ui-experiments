feb43154ff0f1f6953cc316a6c06e440618ac697
anwesh43 <anweshthecool0@gmail.com>
2018-01-23 10:40:17 +0530

    adding NotifUIState

--- NotificationStage.js
+++ NotificationStage.js
@@ -65,3 +65,25 @@ class TextPart {
         context.fillText(this.text,x,this.y)
     }
 }
+class NotifUIState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += 0.1*this.dir
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb(this.prevScale)
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1-2*this.state.scale
+            startcb()
+        }
+    }
+}
