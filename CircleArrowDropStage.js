5bf41f9456458923cb873da80c6909302b5e78c5
anwesh43 <anweshthecool0@gmail.com>
2018-01-24 10:47:37 +0530

    adding CircleArrowDrop

--- CircleArrowDropStage.js
+++ CircleArrowDropStage.js
@@ -6,3 +6,38 @@ class CircleArrowDropStage extends CanvasStage{
         super.render()
     }
 }
+class CircleArrowDrop {
+    constructor(x,y,r) {
+        this.x = x
+        this.y = y
+        this.r = r
+    }
+    draw(context) {
+        const r = this.r
+        context.save()
+        context.translate(this.x,this.y)
+        context.beginPath()
+        context.arc(0,0,r,0,2*Math.PI)
+        context.fillStyle = '#E0E0E0'
+        context.fill()
+        context.clip()
+        context.fillStyle = '#4527A0'
+        context.fillRect(-r,-r,2*r,0.4*r+1.6*r)
+        const y = 1.6*r
+        context.save()
+        context.translate(0,y)
+        context.beginPath()
+        context.moveTo(-0.1*r,-0.1*r)
+        context.lineTo(0.1*r,-0.1*r)
+        context.lineTo(0,0.1*r)
+        context.fill()
+        context.restore()
+        context.restore()
+    }
+    update(stopcb) {
+
+    }
+    startUpdating(startcb) {
+
+    }
+}
