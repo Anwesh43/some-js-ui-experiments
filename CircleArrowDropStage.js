6ef3ec7053e84d2f4dc2ab23567394743fbb5bbc
anwesh43 <anweshthecool0@gmail.com>
2018-01-24 11:12:58 +0530

    adding State in CircleArrowDrop

--- CircleArrowDropStage.js
+++ CircleArrowDropStage.js
@@ -11,9 +11,11 @@ class CircleArrowDrop {
         this.x = x
         this.y = y
         this.r = r
+        this.state = new CircleArrowDropState()
     }
     draw(context) {
         const r = this.r
+        const h = 1.1*r*this.state.scale
         context.save()
         context.translate(this.x,this.y)
         context.beginPath()
@@ -22,23 +24,21 @@ class CircleArrowDrop {
         context.fill()
         context.clip()
         context.fillStyle = '#4527A0'
-        context.fillRect(-r,-r,2*r,0.4*r+1.6*r)
-        const y = 1.6*r
+        context.fillRect(-r,-r,2*r,0.1*r+h)
         context.save()
-        context.translate(0,y)
         context.beginPath()
-        context.moveTo(-0.1*r,-0.1*r)
-        context.lineTo(0.1*r,-0.1*r)
-        context.lineTo(0,0.1*r)
+        context.moveTo(-0.1*r,-0.1*r+h)
+        context.lineTo(0.1*r,-0.1*r+h)
+        context.lineTo(0,0.1*r+h)
         context.fill()
         context.restore()
         context.restore()
     }
     update(stopcb) {
-
+        this.state.update(stopcb)
     }
     startUpdating(startcb) {
-
+        this.state.startUpdating(startcb)
     }
 }
 class CircleArrowDropState {
