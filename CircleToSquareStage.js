fd48478a9898237ee3d30fdfaad9162fb9e197a6
anwesh43 <anweshthecool0@gmail.com>
2018-01-31 13:14:05 +0530

    adding CircleToSquareAnimator

--- CircleToSquareStage.js
+++ CircleToSquareStage.js
@@ -63,3 +63,22 @@ class CircleToSquareState {
         }
     }
 }
+class CircleToSquareAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
