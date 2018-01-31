4b72dc29bbbdf8689ee4e0401ad44bbc2db8ea02
anwesh43 <anweshthecool0@gmail.com>
2018-01-31 13:08:10 +0530

    adding CircleToSquareState class

--- CircleToSquareStage.js
+++ CircleToSquareStage.js
@@ -40,3 +40,25 @@ class CircleToSquare {
 
     }
 }
+class CircleToSquareState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb(this.scale)
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.scale = 1 - 2*this.dir
+            startcb()
+        }
+    }
+}
