8de43771c0dddf47fa73db81ac9847c54af44e93
anwesh43 <anweshthecool0@gmail.com>
2018-02-01 09:26:42 +0530

    adding ElasticBallState

--- ElasticBallStage.js
+++ ElasticBallStage.js
@@ -6,3 +6,26 @@ class ElasticBallStage extends CanvasStage {
         super.render()
     }
 }
+class ElasticBallState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.deg = 0
+    }
+    update(stopcb) {
+        this.deg += this.dir*Math.PI/20
+        this.scale = Math.sin(this.deg)
+        if(this.deg > Math.PI) {
+            this.deg = 0
+            this.scale = 0
+            this.dir = 0
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
