e5a014757a1fbdeaac5270496557388f9ecc345a
anwesh43 <anweshthecool0@gmail.com>
2018-02-05 09:16:32 +0530

    adding CTPState

--- CircleToPolygonStage.js
+++ CircleToPolygonStage.js
@@ -6,3 +6,23 @@ class CircleToPolygonStage extends CanvasStage {
         super.render()
     }
 }
+class CTPState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir*0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb(this.scale)
+        }
+    }
+    startUpdating(startcb) {
+        this.dir = 1-2*this.scale
+        startcb()
+    }
+}
