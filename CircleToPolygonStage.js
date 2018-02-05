de12d5c4901bb7a9725305e1ac75e2ae122ec663
anwesh43 <anweshthecool0@gmail.com>
2018-02-05 09:19:38 +0530

    adding CTPAnimator

--- CircleToPolygonStage.js
+++ CircleToPolygonStage.js
@@ -26,3 +26,22 @@ class CTPState {
         startcb()
     }
 }
+class CTPAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(()=>{
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
