e706474a5ff6f8b6b8eaee3a972aabe71d34fbb9
anwesh43 <anweshthecool0@gmail.com>
2018-02-06 11:21:49 +0530

    adding Vector class

--- BouncingBallJoint.js
+++ BouncingBallJoint.js
@@ -6,3 +6,19 @@ class BouncingBallStage extends CanvasStage {
         super.render()
     }
 }
+class Vector {
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+    }
+    add(v) {
+        this.x += v.x
+        this.y += v.y
+    }
+    reverseX() {
+        this.x *= -1
+    }
+    reverseY() {
+        this.y *= -1
+    }
+}
