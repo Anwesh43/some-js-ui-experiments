b13af89648b9dc7001f290b99c6e5435b4efd2a9
anwesh43 <anweshthecool0@gmail.com>
2018-02-07 19:16:54 +0530

    adding fixes

--- ExpandingRoundButtonStage.js
+++ ExpandingRoundButtonStage.js
@@ -1,24 +1,28 @@
 class ExpandingRoundButtonStage extends CanvasStage {
     constructor() {
         super()
-        this.erb = new ERB()
+        const w = this.size.w , h = this.size.h
+        this.erb = new ERB(w/2,h/2,w/12,h/12)
         this.animator = new ERBAnimator()
     }
     render() {
         super.render()
         if(this.erb) {
-            this.erb.draw(context)
+            this.erb.draw(this.context)
         }
     }
     handleTap() {
-        if(this.erb && this.animator) {
-            this.erb.startUpdating(() => {
-                this.animator.start(() => {
-                    this.erb.update(() => {
-                        this.animator.stop()
+        if(this.erb && this.animator && this.canvas) {
+            this.canvas.onmousedown = () => {
+                this.erb.startUpdating(() => {
+                    this.animator.start(() => {
+                        this.render()
+                        this.erb.update(() => {
+                            this.animator.stop()
+                        })
                     })
                 })
-            })
+            }
         }
     }
 }
@@ -51,7 +55,7 @@ class ERBAnimator {
     start(updatecb) {
         if(!this.animated) {
             this.animated = true
-            setInterval(() => {
+            this.interval = setInterval(() => {
               updatecb()
             },50)
         }
@@ -72,6 +76,7 @@ class ERB {
         this.state = new ERBState()
     }
     draw(context) {
+        context.fillStyle = 'yellowgreen'
         const scale = this.state.scale
         context.save()
         context.translate(this.x,this.y)
@@ -83,9 +88,9 @@ class ERB {
         context.fill()
         context.beginPath()
         for(var i = 0;i<2;i++) {
-            const px = (this.w/3) * (1-2*i)
-            for(var j = 180*i,j <= 180*i+180;j++) {
-                const x = px+this.h/2*Math.cos(j*Math.PI/180), y = this.h/2*Math.sin(j*Math.PI/180)
+            const px = (this.w/3-this.w/20) * (1-2*i) * scale
+            for(var j = 180*i; j <= 180*i+180;j++) {
+                const x = px+this.h/2*Math.cos(j*Math.PI/180 - Math.PI/2), y = this.h/2*Math.sin(j*Math.PI/180 - Math.PI/2)
                 if(j == 180*i) {
                     context.moveTo(x,y)
                 }
