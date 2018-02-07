09cdd0a7db6481ebbb35618ca956ddedc68b1a08
anwesh43 <anweshthecool0@gmail.com>
2018-02-07 09:48:19 +0530

    adding ERBState

--- ExpandingRoundButtonStage.js
+++ ExpandingRoundButtonStage.js
@@ -6,3 +6,25 @@ class ExpandingRoundButtonStage extends CanvasStage {
         super.render()
     }
 }
+class ERBState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb(this.scale)
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2 * this.scale
+            startcb()
+        }
+    }
+}
