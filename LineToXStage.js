d01ba00057d42572203ac37c3f1abeb49a4899d8
anwesh43 <anweshthecool0@gmail.com>
2018-02-08 09:46:42 +0530

    adding LTXState

--- LineToXStage.js
+++ LineToXStage.js
@@ -25,3 +25,30 @@ class LTXAnimator {
         }
     }
 }
+class LTXState {
+    constructor() {
+        this.scales = [0,0,0]
+        thsi.dir = 0
+        this.j = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if(Math.abs(this.scales[j] - this.prevScale) > 1) {
+            this.scales[j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.prevScale = this.scales[this.j]
+                this.dir = 0
+                stopcb()
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2*this.prevScale
+            startcb()
+        }
+    } 
+}
