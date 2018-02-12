6ba8e248c7838288e9eb56366da6f2ad54d6d9de
anwesh43 <anweshthecool0@gmail.com>
2018-02-12 10:44:11 +0530

    adding MShapeState

--- MShapeStage.js
+++ MShapeStage.js
@@ -6,3 +6,32 @@ class MShapeStage extends CanvasStage {
         super.render()
     }
 }
+class MShapeState {
+    constructor() {
+        this.scales = [0,0]
+        this.dir = 0
+        this.j = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1*this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.dir *= -1
+                this.j += this.dir
+                if(this.j == 0) {
+                    this.dir = 0
+                    stopcb()
+                }
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
