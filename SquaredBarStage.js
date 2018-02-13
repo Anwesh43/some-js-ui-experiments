f3021c61f0705e34d7deb3b673e25de616a068f5
anwesh43 <anweshthecool0@gmail.com>
2018-02-13 10:08:36 +0530

    adding SBState

--- SquaredBarStage.js
+++ SquaredBarStage.js
@@ -9,3 +9,25 @@ class SquaredBarState extrends CanvasStage {
 
     }
 }
+class SBState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2*this.scale
+            startcb()
+        }
+    }
+}
