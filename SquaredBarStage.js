f331922afbaa9fc2af6a3519bca574c97a361a5f
anwesh43 <anweshthecool0@gmail.com>
2018-02-13 10:40:02 +0530

    adding SquaredBarList

--- SquaredBarStage.js
+++ SquaredBarStage.js
@@ -25,7 +25,7 @@ class SBState {
         }
     }
     startUpdating(startcb) {
-        if(this.dir == 0) {
+        if(this.dir == 0)( {
             this.dir = 1 - 2*this.scale
             startcb()
         }
@@ -48,5 +48,36 @@ class SBAnimator {
             this.animated = true
             clearInterval(this.interval)
         }
+    }
+}
+class SquaredBarList {
+    constructor(w,h) {
+        this.state = new SquaredBarState()
+        this.w = w
+        this.h = h
+    }
+    draw(context) {
+        const n = 10
+        const x_gap = this.w/(n+2), y_gap = this.h/(2*n+1)
+        var x = x_gap, y = y_gap
+        context.save()
+        context.translate(this.w/2,this.h/2)
+        for(var i=0;i<n;i++) {
+            context.beginPath()
+            context.moveTo(0,y_gap)
+            context.lineTo(this.w*this.state.scale,y_gap)
+            context.stroke()
+            context.beginPath()
+            context.moveTo(x_gap,0)
+            context.lineTo(x_gap,this.w*this.state.scale)
+            context.stroke()
+        }
+        context.restore()
+    }
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
     }
 }
