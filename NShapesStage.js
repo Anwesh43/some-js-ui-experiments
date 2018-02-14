a4217b02ceeef61d857240dce727932858dc2028
anwesh43 <anweshthecool0@gmail.com>
2018-02-14 09:47:53 +0530

    adding NSSAnimator

--- NShapesStage.js
+++ NShapesStage.js
@@ -6,6 +6,24 @@ class NShapeStage extends CanvasStage {
         super.render()
     }
     handleTap() {
-        
+
+    }
+}
+class NSSAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+        }
     }
 }
