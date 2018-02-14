3a08ead64dc3079c67e4972274ebf57fa1589730
anwesh43 <anweshthecool0@gmail.com>
2018-02-14 09:54:56 +0530

    adding NSSState class

--- NShapesStage.js
+++ NShapesStage.js
@@ -27,3 +27,33 @@ class NSSAnimator {
         }
     }
 }
+class NSSState {
+    constructor() {
+        this.scales = [0,0,0]
+        this.dir = 0
+        this.prevScale = 0
+        this.j = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1*this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir *= -1
+                this.prevScale = this.scales[this.j]
+                if(this.j == 0) {
+                    this.dir = 0
+                    stopcb()
+                }
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
