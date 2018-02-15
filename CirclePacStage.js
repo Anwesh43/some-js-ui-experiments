d57727a06465d73816157ca9182a1e514af801db
anwesh43 <anweshthecool0@gmail.com>
2018-02-15 09:32:25 +0530

    adding CirclePacAnimator

--- CirclePacStage.js
+++ CirclePacStage.js
@@ -38,3 +38,22 @@ class CirclePacState {
         }
     }
 }
+class CirclePacAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
