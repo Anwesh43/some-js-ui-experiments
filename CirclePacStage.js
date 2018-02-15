f8fea92681106d601d29dbfb35686d9728a9053d
anwesh43 <anweshthecool0@gmail.com>
2018-02-15 09:30:16 +0530

    adding CirclePacState class

--- CirclePacStage.js
+++ CirclePacStage.js
@@ -9,3 +9,32 @@ class CirclePacStage extends CanvasStage {
 
     }
 }
+class CirclePacState {
+    constructor(n) {
+        this.j = 0
+        this.n = 0
+        this.scale = 0
+        this.dir = 0
+        this.deg = 0
+    }
+    update(stopcb) {
+        this.deg += this.dir * Math.PI/15
+        this.scale = Math.sin(this.deg * Math.PI/180)
+        if(this.deg > Math.PI) {
+            this.deg = 0
+            this.j ++
+            if(this.j == this.n) {
+                this.dir = 0
+                this.deg = 0
+                this.scale = 0
+                stopcb()
+            }
+        }
+    }
+    startUpdating() {
+        if(this.dir == 0) {
+            this.dir = 1
+            this.j = 0
+        }
+    }
+}
