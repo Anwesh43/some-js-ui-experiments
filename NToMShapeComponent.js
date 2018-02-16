49d48f38cd8e66ea3a2b5aadabf4ca7de18c0a30
anwesh43 <anweshthecool0@gmail.com>
2018-02-16 11:39:11 +0530

    adding NTSState class

--- NToMShapeComponent.js
+++ NToMShapeComponent.js
@@ -9,3 +9,24 @@ class NToShapeStage extends CanvasStage {
 
     }
 }
+class NTSState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            stopcb(this.scale)
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2*this.scale
+            startcb()
+        }
+    }
+}
