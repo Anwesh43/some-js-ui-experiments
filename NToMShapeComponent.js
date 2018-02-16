3746370c2685aee4c3ba419efc9751495f2978b5
anwesh43 <anweshthecool0@gmail.com>
2018-02-16 22:01:44 +0530

    adding NToMShape

--- NToMShapeComponent.js
+++ NToMShapeComponent.js
@@ -49,3 +49,44 @@ class NTSAnimator {
         }
     }
 }
+class NToMShape {
+    constructor(w,h) {
+        this.w = w
+        this.h = h
+        this.size = 2*Math.min(w,h)/3
+        this.state = new NTSState()
+    }
+    draw(context) {
+        const size = this.size, x = this.x, y = this.y
+        context.lineWidth = size/20
+        context.strokeStyle = '#27ae60'
+        context.lineCap = 'round'
+        context.save()
+        context.translate(x,y)
+        const x_gap = (size/2) * Math.sin(Math.PI/6), y_gap = (size/2)*Math.cos(Math.PI/6)
+        for(var i = 0;i< 2; i++) {
+            const factor = (2*i-1)
+            context.save()
+            context.translate(x_gap*factor,0)
+            context.beginPath()
+            context.moveTo(0, -y_gap)
+            context.lineTo(0, y_gap)
+            context.stroke()
+            context.save()
+            context.rotate(Math.PI/3*-factor + (2*Math.PI/3)*i*this.state.scale)
+            context.beginPath()
+            context.moveTo(0,0)
+            context.lineTo(size/2*factor,0)
+            context.stroke()
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
+    }
+}
