613a5b355fd06df1ea11ff02731ae3830965ed25
anwesh43 <anweshthecool0@gmail.com>
2018-02-19 11:48:26 +0530

    adding LineBallState class

--- LineBallStage.js
+++ LineBallStage.js
@@ -9,3 +9,35 @@ class LineBallStage extends CanvasStage {
 
     }
 }
+class LineBallState {
+    constructor() {
+        this.scales = [0,0,0]
+        this.prevScale = 0
+        this.dir = 0
+        this.j = 0
+    }
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if(Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.size || this.j == -1) {
+                this.dir *= -1
+                this.j += this.dir
+                if(this.dir == -1) {
+                    this.j += this.dir
+                }
+                else {
+                    this.dir = 0
+                    stopcb()
+                }
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
