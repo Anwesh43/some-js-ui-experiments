1db1f1ae37e42ce1271c496c0e92b3fc9a1e1973
anwesh43 <anweshthecool0@gmail.com>
2018-02-20 09:19:16 +0530

    adding FollowMouseAnimator

--- FollowMouseStage.js
+++ FollowMouseStage.js
@@ -40,3 +40,22 @@ class FollowMouseState {
         }
     }
 }
+class FollowMouseAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
