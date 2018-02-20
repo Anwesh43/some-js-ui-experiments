ea2be63f230317675d0c005a130e1b54177e3983
anwesh43 <anweshthecool0@gmail.com>
2018-02-20 09:08:54 +0530

    adding FollowMouseStage

---
+++ FollowMouseStage.js
@@ -0,0 +1,13 @@
+class FollowMouseStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+    render() {
+        super.render()
+    }
+    handleTap() {
+        this.canvas.onmousedown = (event) => {
+            const x = event.offsetX, y = event.offsetY
+        }
+    }
+}
