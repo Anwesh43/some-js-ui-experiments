a6a79db77b1f9abcab44519e0b27b131dc93237c
anwesh43 <anweshthecool0@gmail.com>
2018-02-21 19:09:58 +0530

    adding SquareMoverState

--- SquareMoverStage.js
+++ SquareMoverStage.js
@@ -9,3 +9,28 @@ class SquareMoverStage extends CanvasStage {
 
     }
 }
+class SquareMoverState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.j = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if(Math.abs(this.scales[this.j]) > 1) {
+            this.scales[this.j]  = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length) {
+                this.dir = 0
+                this.j = 0
+                stopcb()
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            this.scales = [0, 0]
+        }
+    }
+}
