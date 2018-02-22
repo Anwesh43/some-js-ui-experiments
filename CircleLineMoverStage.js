238c0e98516a53bbb2d38a5fbe8c1f056cb24225
anwesh43 <anweshthecool0@gmail.com>
2018-02-22 09:28:25 +0530

    adding State class

--- CircleLineMoverStage.js
+++ CircleLineMoverStage.js
@@ -30,3 +30,28 @@ class CircleLineAnimator {
         }
     }
 }
+class CircleLineState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.j = 0
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.scales = [0,0]
+            this.dir = 1
+            startcb()
+        }
+    }
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if(this.scales[this.j] > 1) {
+            this.scales[this.j] = 1
+            if(this.j == this.scales.length) {
+                this.dir = 0
+                this.j = 0
+                stopcb()
+            }
+        }
+    }
+}
