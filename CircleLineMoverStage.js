c50025525eeec78a1b227844f0ec8da5d029624d
anwesh43 <anweshthecool0@gmail.com>
2018-02-22 09:22:19 +0530

    adding CircleLineAnimator

--- CircleLineMoverStage.js
+++ CircleLineMoverStage.js
@@ -11,3 +11,22 @@ class CircleLineMoverStage extends CanvasStage {
         }
     }
 }
+class CircleLineAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
