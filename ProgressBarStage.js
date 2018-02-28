7321c5b1e460f58da40ce38245c17f1d4486243e
anwesh43 <anweshthecool0@gmail.com>
2018-02-28 10:01:17 +0530

    adding Stage class

--- ProgressBarStage.js
+++ ProgressBarStage.js
@@ -9,3 +9,30 @@ class ProgressBarStage extends CanvasStage {
 
     }
 }
+class State {
+    constructor() {
+        this.scales = [0, 0, 0]
+        this.prevScale = 0
+        this.dir = 0
+        this.j = 0
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if(Math.abs(this.scales[this.j] > this.prevScale)) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.prevScale = this.scales[this.j] 
+                this.dir = 0
+                stopcb()
+            }
+        }
+    }
+}
