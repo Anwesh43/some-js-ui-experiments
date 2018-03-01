63a73c03f2499c8ef0d9a5e957409ae4636a4925
anwesh43 <anweshthecool0@gmail.com>
2018-03-01 09:25:33 +0530

    adding TCMState

--- TapCircleMoverStage.js
+++ TapCircleMoverStage.js
@@ -6,6 +6,32 @@ class TapCircleMoverStage extends CanvasStage {
         super.render()
     }
     handleTap() {
-        
+
+    }
+}
+class TCMState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.j = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if(this.scales[this.j] > 1) {
+            this.scales[this.j] = 1
+            this.j++
+            if(this.j == this.scales.length) {
+                this.j = 0
+                this.dir = 0
+                stopcb()
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            this.scales = [0,0]
+            startcb()
+        }
     }
 }
