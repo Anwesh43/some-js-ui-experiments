9c91208a2635bfb68f3c51e0efe5d9308767fbc4
anwesh43 <anweshthecool0@gmail.com>
2018-03-02 09:13:53 +0530

    adding State class

--- RectTextBarStage.js
+++ RectTextBarStage.js
@@ -9,3 +9,35 @@ class RectTextBarStage extends CanvasStage {
 
     }
 }
+class State {
+    constructor(n) {
+        this.scales = []
+        this.prevScale = 0
+        this.j = 0
+        this.init(n)
+    }
+    init(n) {
+        for(var i = 0; i < n; i++) {
+            this.scales.push(0)
+        }
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
