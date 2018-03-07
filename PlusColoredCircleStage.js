d187613af6e2cf80f4b364086584e5ac3f490029
anwesh43 <anweshthecool0@gmail.com>
2018-03-07 10:36:26 +0530

    adding State class

--- PlusColoredCircleStage.js
+++ PlusColoredCircleStage.js
@@ -12,3 +12,33 @@ class PlusColoredCircleStage extends CanvasStage {
         }
     }
 }
+class State {
+    constructor() {
+        this.scales = [0, 0]
+        this.j = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir *= -1
+                this.prevScale = this
+                if(this.dir == 1) {
+                    this.dir = 0
+                    stopcb()
+                }
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
