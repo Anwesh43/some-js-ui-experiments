921dc6b26e929c7f1a0ee4fba07d85161d58111f
anwesh43 <anweshthecool0@gmail.com>
2018-03-07 11:02:36 +0530

    adding PlusColoredCircle

--- PlusColoredCircleStage.js
+++ PlusColoredCircleStage.js
@@ -61,3 +61,62 @@ class Animator {
         }
     }
 }
+class PlusColoredCircle {
+    constructor(x, y) {
+        this.state = new State()
+        this.x = x
+        this.y = y
+    }
+    drawPlus(context, size) {
+        const scale1 = this.state.scales[0], scale2 = this.state.scales[1], updated_size = size * scale1
+        context.save()
+        context.rotate(Math.PI/4 * scale2)
+        for(var i = 0; i < 2; i++) {
+            context.save()
+            context.rotate(i * Math.PI/2)
+            context.beginPath()
+            context.moveTo(0, -updated_size)
+            context.lineTo(0, updated_size)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    drawArc(context, color, r, start, sweep) {
+        context.strokeStyle = color
+        context.save()
+        context.rotate(start * Math.PI/180)
+        context.beginPath()
+        for(var i = 0; i <= sweep; i++) {
+            const x = r * Math.cos(i * Math.PI/180), y = r * Math.sin(i * Math.PI/180)
+            if(i == 0) {
+                context.moveTo(x, y)
+            }
+            else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+        context.restore()
+    }
+    drawColoredCircle(context, r) {
+        const colors = ["#3498db", "#9b59b6", "#1abc9c", "#e74c3c", "#27ae60", "#e67e22"]
+        const deg = 360/colors.length
+        colors.forEach((color, index) => {
+            this.drawArc(context, color, r, index * deg, deg)
+        })
+    }
+    draw(context, size) {
+        context.save()
+        context.tranlsate(this.x, this.y)
+        this.drawPlus(context, size/3)
+        this.drawColoredCircle(context, size/2)
+        context.restore()
+    }
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
+    }
+}
