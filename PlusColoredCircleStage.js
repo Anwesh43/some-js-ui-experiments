5d129b15bba95e8c725705b98c8f48fa7cbbe80c
anwesh43 <anweshthecool0@gmail.com>
2018-03-07 10:43:01 +0530

    adding Animator class

--- PlusColoredCircleStage.js
+++ PlusColoredCircleStage.js
@@ -42,3 +42,22 @@ class State {
         }
     }
 }
+class Animator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
