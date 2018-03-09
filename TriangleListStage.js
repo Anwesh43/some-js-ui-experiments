72a76bed30c046f4c25860c4a3c8d13f0ef1e562
anwesh43 <anweshthecool0@gmail.com>
2018-03-09 13:44:16 +0530

    adding TriangleListAnimator class

--- TriangleListStage.js
+++ TriangleListStage.js
@@ -38,3 +38,22 @@ class TriangleListState {
         }
     }
 }
+class TriangleListAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if (this.animated) {
+              this.animated = false
+              clearInterval(this.interval)
+        }
+    }
+}
