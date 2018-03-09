7da091505d92752ad6fb2ea0afd798b88998b321
anwesh43 <anweshthecool0@gmail.com>
2018-03-09 20:03:14 +0530

    adding TriangleList

--- TriangleListStage.js
+++ TriangleListStage.js
@@ -57,3 +57,38 @@ class TriangleListAnimator {
         }
     }
 }
+class TriangleList {
+    constructor(size) {
+        this.size = size
+        this.state = new TriangleListState()
+    }
+    draw(context) {
+        const k = 10
+        for (var i = 0; i < k; i++) {
+            context.save()
+            context.translate((i - k/2) * size, 0)
+            this.drawTriangle(context)
+            context.restore()
+        }
+    }
+    drawTriangle(context) {
+        context.save()
+        context.translate(0, -this.size)
+        for(var i = 0; i < 2; i++) {
+            context.save()
+            context.rotate(this.state.scales[1] * Math.PI/4)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, size * Math.sqrt(2) * this.state.scales[0])
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
+    }
+}
