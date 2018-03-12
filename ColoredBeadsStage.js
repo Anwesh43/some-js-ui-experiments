41883722c974089f244edd5daf19fd1e84cc3e96
anwesh43 <anweshthecool0@gmail.com>
2018-03-12 09:25:23 +0530

    adding ColoredBeadState class

--- ColoredBeadsStage.js
+++ ColoredBeadsStage.js
@@ -11,3 +11,30 @@ class ColoredBeadsStage extends CanvasStage {
         }
     }
 }
+class ColoredBeadState {
+    constructor() {
+        this.scales = [0, 0, 0]
+        this.prevScale = 0
+        this.dir = 0
+        this.j = 0
+    }
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.prevScale = this.scales[this.j]
+                this.dir = 0
+                stopcb()
+            }
+        }
+    }
+}
