46e049485720be940938000b1af939fdfaba1e83
anwesh43 <anweshthecool0@gmail.com>
2018-03-12 09:28:43 +0530

    adding ColoredBeadAnimator class

--- ColoredBeadsStage.js
+++ ColoredBeadsStage.js
@@ -38,3 +38,22 @@ class ColoredBeadState {
         }
     }
 }
+class ColoredBeadAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() =>  {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
