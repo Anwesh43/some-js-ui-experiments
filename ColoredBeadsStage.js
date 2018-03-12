d23a78608301248b41a0ef7a90f4bea636e0c4ab
anwesh43 <anweshthecool0@gmail.com>
2018-03-12 09:18:24 +0530

    adding ColoredBeadStage

---
+++ ColoredBeadsStage.js
@@ -0,0 +1,13 @@
+class ColoredBeadsStage extends CanvasStage {
+    constructor () {
+        super()
+    }
+    render() {
+        super.render()
+    }
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
