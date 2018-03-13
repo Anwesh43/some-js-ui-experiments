0a54054978d4a64274a856271139ea291dc5d2ff
anwesh43 <anweshthecool0@gmail.com>
2018-03-13 11:07:19 +0530

    adding MustacheState

--- MustacheStage.js
+++ MustacheStage.js
@@ -9,3 +9,25 @@ class MustacheStage extends CanvasStage {
 
     }
 }
+class MustacheState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+        this.deg = 0
+    }
+    update() {
+      this.deg += this.dir * Math.PI / 20
+      this.scale = Math.sin(this.deg)
+      if (this.deg > Math.PI) {
+          this.deg = 0
+          this.dir = 0
+      }
+    }
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
