2c98e6f518b7b3bb03c2694f73bff7eb8045176a
anwesh43 <anweshthecool0@gmail.com>
2018-03-15 11:54:25 +0530

    adding FlyBallState

--- FlyBallContainerStage.js
+++ FlyBallContainerStage.js
@@ -9,3 +9,26 @@ class FlyBallContainerStage extends CanvasStage {
 
     }
 }
+class FlyBallState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.deg = 0
+    }
+    update(stopcb) {
+        this.deg += Math.PI/20 * this.dir
+        this.scale = Math.sin(this.deg)
+        if (this.deg > 2 * Math.PI) {
+            this.dir = 0
+            this.scale = 0
+            this.deg = 0
+            stopcb()
+        }
+    }
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
