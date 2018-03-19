80f8153b808cb46a33681f8887aa2b7a6dff1cf1
anwesh43 <anweshthecool0@gmail.com>
2018-03-19 10:31:16 +0530

    adding ArrowMoverState

--- ArrowMoverStage.js
+++ ArrowMoverStage.js
@@ -6,6 +6,32 @@ class ArrowMoverStage extends CanvasStage {
         super.render()
     }
     handleTap() {
-        
+
+    }
+}
+class ArrowMoverState {
+    constructor() {
+        this.scales = [0,0,0,0]
+        this.dir = 0
+        this.j = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scales[this.j]) > 1) {
+            this.scales[this.j] = this.dir
+            this.j++
+            if (this.j == this.scales.length) {
+                this.j = 0
+                this.dir = 0
+                stopcb()
+            }
+        }
+    }
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1
+            this.j = 0
+            startcb()
+        }
     }
 }
