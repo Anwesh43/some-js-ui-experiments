98c1be47d04bc0fd1940667c8fe3ddd1b9d820d3
anwesh43 <anweshthecool0@gmail.com>
2018-03-19 11:54:20 +0530

    adding ArrowMover class

--- ArrowMoverStage.js
+++ ArrowMoverStage.js
@@ -15,11 +15,16 @@ class ArrowMoverState {
         this.dir = 0
         this.j = 0
     }
-    update(stopcb) {
+    update(stopcb,midcb) {
         this.scales[this.j] += 0.1 * this.dir
         if (Math.abs(this.scales[this.j]) > 1) {
             this.scales[this.j] = this.dir
             this.j++
+            if(this.j == 2) {
+                midcb(() => {
+                    this.j +=2
+                })
+            }
             if (this.j == this.scales.length) {
                 this.j = 0
                 this.dir = 0
@@ -52,5 +57,57 @@ class ArrowMoverAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+class ArrowMover {
+    constructor() {
+        this.state = new ArrowMoverState()
+    }
+    draw(context, w, h) {
+        if (!this.x && !this.y) {
+            this.dx = this.x
+            this.dy = this.y
+            this.ox = this.dx
+            this.oy = this.dy
+            this.rotX = 0
+            this.rotY = 0
+        }
+        context.strokeStyle = 'white'
+        context.lineWidth = Math.min(w, h)/50
+        context.lineCap = 'round'
+        context.save()
+        const updateXY  = (o, d, i) => o + (d - o) * this.state.scales[i]
+        context.translate(updateXY(this.ox, this.dx,1), updateXY(this.oy, this.dy, 3))
+        context.rotate(this.rotX * this.state.scales[0] + this.rotY * this.state.scales[2])
+        const size = Math.min(w,h)/20
+        for(var i = 0; i< 2; i++) {
+            context.beginPath()
+            context.moveTo(0,0)
+            context.lineTo(-size, (size/2) * (1 - 2 * i))
+            context.stroke()
+        }
+        context.restore()
+    }
+    update(stopcb) {
+        this.state.update(() => {
+            this.ox = this.dx
+            this.oy = this.dy
+            stopcb()
+        }, (skipcb) => {
+            this.rotY = 0
+            if (this.ox != this.dx) {
+                this.rotX = (1 - Math.floor((this.dx - this.ox)/Math.abs(this.ox - this.dx))) * (Math.PI/2)
+            }
+            else {
+                skipcb()
+            }
+        })
+    }
+    startUpdating(startcb, x, y) {
+        this.state.startUpdating(() => {
+            this.dx = x
+            this.dy = y
+            startcb()
+        })
     }
 }
