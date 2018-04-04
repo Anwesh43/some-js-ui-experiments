98b193d7b51d0cc727c69d76c89a66a9becf3edc
anwesh43 <anweshthecool0@gmail.com>
2018-04-04 10:43:06 +0530

    adding StepLiner class

--- StepLinerStage.js
+++ StepLinerStage.js
@@ -1,3 +1,5 @@
+const STEP_LINERS = 11
+
 class StepLinerStage extends CanvasStage {
     constructor() {
         super()
@@ -52,5 +54,31 @@ class StepLinerAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class StepLiner {
+    constructor(i) {
+        this.state = new StepLinerState()
+        this.i = i
+    }
+    draw(context, w, h) {
+        const n = Math.floor(STEP_LINERS/2), k = Math.floor((this.i)/n)
+        const y_gap = (h / STEP_LINERS + 2), x_gap = (w/STEP_LINERS)
+        var i = this.i * (1 - k) + k * (n - this.i)
+        const x = (w - x_gap / 2) - (x_gap * i)
+        const y = y_gap + (y_gap * this.i)
+        context.save()
+        context.translate(x, y)
+        context.moveTo(-(x_gap/2) * this.state.scale, 0)
+        context.lineTo((x_gap/2) * this.state.scale, 0)
+        context.stroke()
+        context.restore()
+    }
+    update (stopcb) {
+        this.state.update(stopcb)
+    }
+    startUpdating (startcb) {
+        this.state.startUpdating(startcb)
     }
 }
