fe754878a9ff7aa90a6f468b98ca65ba58d59ce3
anwesh43 <anweshthecool0@gmail.com>
2018-04-06 09:11:05 +0530

    adding SWMState class

--- SineWaveMoverStage.js
+++ SineWaveMoverStage.js
@@ -9,3 +9,21 @@ class SineWaveMoverStage extends CanvasStage {
 
     }
 }
+class SWMState {
+    constructor (w, h) {
+        this.scale = 0
+        this.deg = 0
+        this.limit = w
+        this.amp = h
+    }
+    update(stopcb) {
+        this.deg += 10
+        this.scale = this.amp * Math.sin(this.deg * Math.PI/180)
+        if (this.deg > this.limit) {
+            stopcb()
+        }
+    }
+    execute(cb) {
+        cb(this.deg, this.scale)
+    }
+}
