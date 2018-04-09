af571b98e1a1540e4471b580979b6c04a3475c58
anwesh43 <anweshthecool0@gmail.com>
2018-04-09 15:01:57 +0530

    adding RailLine class

--- RailLineStage.js
+++ RailLineStage.js
@@ -26,7 +26,7 @@ class RailLineState {
             stopcb()
         }
     }
-
+(
     startUpdating(startcb) {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
@@ -54,5 +54,46 @@ class RailLineAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class RailLine {
+    constructor () {
+        this.state = new RailLineState()
+    }
+    draw (context, w, h) {
+        const r = Math.min(w, h)/6
+        context.save()
+        context.translate(w/2, h/2)
+        context.fillStyle = '#95a5a6'
+        context.beginPath()
+        context.arc(0, 0, r, 0, 2 * Math.PI)
+        context.fill()
+        context.fillStyle = '#212121'
+        context.beginPath()
+        context.arc(0, 0, r * this.state.scale, 0, 2 * Math.PI)
+        context.fill()
+        const x_gap = r/5
+        var x = -3 * r + 3 * r * this.state.scale
+        context.save()
+        context.beginPath()
+        context.rect(0,h/2,w,h/2)
+        context.clipPath()
+        for (var i = 0; i < 20; i++) {
+
+            context.beginPath()
+            context.moveTo(x * Math.cos(Math.PI/3), x * Math.sin(Math.PI/3))
+            context.lineTo(x * Math.cos(2 * Math.PI/3), x * Math.sin(2 * Math.PI/3))
+            context.stroke()
+            x += x_gap
+        }
+        context.restore()
+        context.restore()
+    }
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
     }
 }
