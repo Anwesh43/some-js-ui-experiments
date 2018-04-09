420a0f293be3c2f304c37bdbd3df62aa78345ede
anwesh43 <anweshthecool0@gmail.com>
2018-04-09 12:53:13 +0530

    adding RailLineAnimator class

--- RailLineStage.js
+++ RailLineStage.js
@@ -34,3 +34,25 @@ class RailLineState {
         }
     }
 }
+
+class RailLineAnimator {
+    constructor () {
+        this.animated = false
+    }
+
+    start (updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = clearInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
