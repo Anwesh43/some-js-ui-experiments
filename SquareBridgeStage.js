d544165f47b3552a1495c60342df7f00b0c267b8
anwesh43 <anweshthecool0@gmail.com>
2018-04-18 14:10:42 +0530

    adding SQSAnimator class

--- SquareBridgeStage.js
+++ SquareBridgeStage.js
@@ -37,3 +37,23 @@ class SQSState {
         }
     }
 }
+
+class SQSAnimator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
