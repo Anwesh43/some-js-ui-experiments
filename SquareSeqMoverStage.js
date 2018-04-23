fc26d8ed481c3165d3c5c192b604bdcad08bf187
anwesh43 <anweshthecool0@gmail.com>
2018-04-23 12:55:16 +0530

    adding SSMState class

--- SquareSeqMoverStage.js
+++ SquareSeqMoverStage.js
@@ -14,3 +14,36 @@ class SquareSeqMoverStage extends CanvasStage {
         }
     }
 }
+
+class SSMState {
+
+    constructor(n) {
+        this.scales = [0, 0]
+        this.prevScale = 0
+        this.dir = 0
+        this.j = 0
+        this.k = 0
+        this.n = n
+    }
+
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.j += this.dir
+            this.scales[this.j] = this.prevScale + this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
