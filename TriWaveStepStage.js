1b118b335930731b8d14e8b51342f1da0bf8c5cb
anwesh43 <anweshthecool0@gmail.com>
2018-04-26 10:18:58 +0530

    adding TriWaveStepStage

---
+++ TriWaveStepStage.js
@@ -0,0 +1,16 @@
+class TriWaveStepStage extends HTMLElement {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = (event) => {
+
+        }
+    }
+}
