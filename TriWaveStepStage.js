c4a77e7e1ae3970d98ee242d875a7ebfc0e6bc02
anwesh43 <anweshthecool0@gmail.com>
2018-04-26 11:01:08 +0530

    adding TWSNode class

--- TriWaveStepStage.js
+++ TriWaveStepStage.js
@@ -1,3 +1,4 @@
+const NUM_TRI_WAVES = 10
 class TriWaveStepStage extends HTMLElement {
 
     constructor() {
@@ -65,6 +66,47 @@ class TWSAnimator {
         if (this.animated) {
             this.animated = false
             clearInterval(this.interval)
+        }
+    }
+}
+
+class TWSNode {
+
+    constructor(i) {
+        this.state = new TWSState()
+        this.i = 0
+        if (i) {
+            this.i = i
+        }
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
+    }
+
+    draw(context, w, h) {
+        const gap = w / NUM_TRI_WAVES
+        const gap1 = (gap/2) * this.state.scales[0], gap2 = (gap/2) * this.state.scales[1], gap3 = (gap/2) * this.state.scales[2]
+        context.save()
+        context.translate(i * gap , h/2)
+        context.beginPath()
+        context.moveTo(gap2, -gap2)
+        context.lineTo(gap1 + gap3, -gap1 + gap3)
+        context.lineTo(gap1 + gap2, -gap1 + gap2)
+        context.stroke()
+        context.restore()
+    }
+
+    addNeighbor() {
+        if (this.i < NUM_TRI_WAVES -1) {
+            const NODE = new TWSNode(this.i+1)
+            NODE.prev = this
+            this.next = NODE
+            NODE.addNeighbor()
         }
     }
 }
