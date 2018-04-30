7f15318c2603bd3cb14003e64f743506fa112f35
anwesh43 <anweshthecool0@gmail.com>
2018-04-30 10:23:02 +0530

    addinG ALNode class

--- ArcLinkedStage.js
+++ ArcLinkedStage.js
@@ -1,3 +1,5 @@
+const ALNODES = 5
+
 class ArcLinkedStage extends CanvasStage {
 
     constructor() {
@@ -66,5 +68,68 @@ class ALAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class ALNode {
+    constructor(i) {
+        this.state = new ALState()
+        this.i = 0
+        if (i) {
+            this.i = i
+        }
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < ALNODES - 1) {
+            const NODE = new ALNode(this.i + 1)
+            this.next = NODE
+            NODE.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+      var start = 180 + Math.floor(180 * this.state.scales[0]), end = 180 + Math.floor(180 * this.state.scales[0])
+        const size = w / ALNODES, r = size/2
+        context.save()
+        context.translate(this.i * size + r, h/2)
+        for (var i = 0; i < 2; i++) {
+            context.save()
+            context.scale(1, 1 - 2 * i)
+            context.beginPath()
+            for (var j = start; j <= end; j++) {
+                const x = Math.cos(j * Math.PI/180), y = Math.sin(j * Math.PI/180)
+                if (j == start) {
+                    context.moveTo(x, y)
+                }
+                else {
+                    context.lineTo(x, y)
+                }
+            }
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
     }
 }
