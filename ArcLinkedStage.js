94453dcb17760493a4c2fc8e682f27d36787e18f
anwesh43 <anweshthecool0@gmail.com>
2018-04-30 08:54:24 +0530

    adding ArcLinkedStage class

---
+++ ArcLinkedStage.js
@@ -0,0 +1,16 @@
+class ArcLinkedStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
