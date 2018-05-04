357c713baf80e339c328b64637d98a1d946a8446
anwesh43 <anweshthecool0@gmail.com>
2018-05-04 12:08:19 +0530

    adding RotatingLinkedLineStage

---
+++ RotatingLinkedLineStage.js
@@ -0,0 +1,16 @@
+class RotatingLinkedLineStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
