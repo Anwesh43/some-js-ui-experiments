1c2e7edf9590f55a5d5f3f8a8006563ef00e6e10
anwesh43 <anweshthecool0@gmail.com>
2018-05-04 12:27:36 +0530

    adding RLLState class

--- RotatingLinkedLineStage.js
+++ RotatingLinkedLineStage.js
@@ -14,3 +14,33 @@ class RotatingLinkedLineStage extends CanvasStage {
         }
     }
 }
+
+class RLLState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.j = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scales[this.j] += 0.1
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
