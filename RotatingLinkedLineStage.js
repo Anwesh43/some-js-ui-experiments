17b3e3e37b846178b37987724ec6253ce07b893d
anwesh43 <anweshthecool0@gmail.com>
2018-05-04 12:30:36 +0530

    adding RLLAnimator class

--- RotatingLinkedLineStage.js
+++ RotatingLinkedLineStage.js
@@ -44,3 +44,25 @@ class RLLState {
         }
     }
 }
+
+class RLLAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
