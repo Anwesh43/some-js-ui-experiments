caccb9aaef70ae24be44ec03f3b47d7b848b23b4
anwesh43 <anweshthecool0@gmail.com>
2018-05-08 12:44:55 +0530

    adding BinaryTreeBallStage class

---
+++ BinaryTreeBallStage.js
@@ -0,0 +1,16 @@
+class BinaryTreeBallStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
