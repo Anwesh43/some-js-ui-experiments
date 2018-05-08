5d095e5f187b1b4f49a8f84c88e4b19250e11838
anwesh43 <anweshthecool0@gmail.com>
2018-05-08 14:09:17 +0530

    adding BTBNode class

--- BinaryTreeBallStage.js
+++ BinaryTreeBallStage.js
@@ -1,3 +1,4 @@
+const LEVELS = 5
 class BinaryTreeBallStage extends CanvasStage {
 
     constructor() {
@@ -61,5 +62,49 @@ class BTBAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class BTBNode {
+
+    constructor(i, x, y, gap) {
+        this.x = x
+        this.y = y
+        this.i = i
+        this.addChildren(gap)
+    }
+    addParent(parent) {
+        this.parent = parent
+        this.state = new BTBState()
+    }
+    addChildren(gap) {
+        if (this.i < LEVELS -1) {
+            const left = new BTBNode(this.i+1, this.x - gap, y + gap, gap)
+            const right = new BTBNode(this.i+1, this.x - gap, y + gap, gap)
+            left.addParent(parent)
+            right.addParent(parent)
+            this.left = left
+            this.right = right
+        }
+    }
+
+    draw(context, w , h) {
+        var x = this.x, y = this.y, r = Math.min(w,h) / (LEVELS * 3)
+        if (this.parent) {
+            x = this.parent.x + (this.x - this.parent.x) * this.state.scale
+            y = this.parent.y + (this.y - this.parent.y) * this.state.scale
+        }
+        context.fillStyle = '#e67e22'
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
     }
 }
