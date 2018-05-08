fb3ef4ead4b9ffbc40b46b24a5a1bbb07ff7db38
anwesh43 <anweshthecool0@gmail.com>
2018-05-08 12:49:07 +0530

    adding BTBState class

--- BinaryTreeBallStage.js
+++ BinaryTreeBallStage.js
@@ -14,3 +14,29 @@ class BinaryTreeBallStage extends CanvasStage {
         }
     }
 }
+
+class BTBState {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
