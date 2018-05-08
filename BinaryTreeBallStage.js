2645c87af082696ae59935b9695f82c69bb67d9e
anwesh43 <anweshthecool0@gmail.com>
2018-05-08 12:53:03 +0530

    adding BTBAnimator class

--- BinaryTreeBallStage.js
+++ BinaryTreeBallStage.js
@@ -40,3 +40,26 @@ class BTBState {
         }
     }
 }
+
+class BTBAnimator {
+
+    constructor() {
+        this.animated = false
+    }
+
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
