3b61c30633f1981f19866bc6c68e24412e9b07ef
anwesh43 <anweshthecool0@gmail.com>
2018-05-09 20:09:35 +0530

    adding FNNode class

--- TreeFolderStage.js
+++ TreeFolderStage.js
@@ -1,3 +1,4 @@
+const FN_LEVEL = 4
 class TreeFolderStage extends CanvasStage {
     constructor() {
         super()
@@ -57,6 +58,71 @@ class FNAnimator {
         if (this.animated) {
             this.animated = false
             clearInterval(this.interval)
+        }
+    }
+}
+
+class FNNode {
+
+    constructor(i, x, y, ew, eh) {
+        this.x = x
+        this.y = y
+        this.i = i
+        this.children = []
+        this.addChildren(ew, eh)
+        this.state = new FNState()
+    }
+
+    addChildren(ew, eh) {
+        if (this.i < FN_LEVEL - 1) {
+            for (var i = 0; i < 3; i++) {
+                const fnNode = new FNNode(this.i+1, this.x + ew, this.x + eh * i ,ew, eh)
+                this.children.push(fnNode)
+            }
+        }
+    }
+
+    draw(context, px, py) {
+        const x = px + (this.x - px) * this.state.scale,y = py + (this.y - py) * this.state.scale
+        context.moveTo(px, py)
+        context.lineTo(x, y)
+        context.stroke()
+        context.save()
+        context.translate(x, y)
+        context.beginPath()
+        context.arc(0, 0, r, 0, 2 * Math.PI)
+        context.fill()
+        context.restore()
+        this.children.forEach((child) => {
+            child.draw(context, this.x, this.y)
+        })
+    }
+
+    update(stopcb, dir) {
+        this.state.update(() => {
+            if (this.children.length > 0 || dir == 1) {
+                this.children.startUpdating(() => {
+                    stopcb()
+                })
+            }
+            else {
+                stopcb()
+            }
+        })
+    }
+
+    startUpdating(startcb) {
+        if (this.i == 0) {
+            this.childrens.forEach((child,index) => {
+                child.startUpdating(() => {
+                    if (index == this.childrens.length - 1) {
+                        startcb()
+                    }
+                })
+            })
+        }
+        else {
+            startcb()
         }
     }
 }
