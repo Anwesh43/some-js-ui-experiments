4296e306db94786e68a49b5dae0614bd5d880ca4
anwesh43 <anweshthecool0@gmail.com>
2018-05-09 17:25:41 +0530

    adding Animator class

--- TreeFolderStage.js
+++ TreeFolderStage.js
@@ -38,3 +38,25 @@ class FNState {
         }
     }
 }
+
+class FNAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
