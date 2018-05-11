6f1ab695a9c9c4c6de833738e081d784cef8b776
anwesh43 <anweshthecool0@gmail.com>
2018-05-11 16:21:52 +0530

    adding TRLLState class

--- TRotatingLinkedLineStage.js
+++ TRotatingLinkedLineStage.js
@@ -14,3 +14,34 @@ class TRotatingLinkedLineStage extends CanvasStage {
         }
     }
 }
+
+class TRLLState {
+
+    constructor() {
+        this.scales = [0, 0, 0, 0]
+        this.dir = 0
+        this.prevScale = 0
+        this.j = 0
+    }
+
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
