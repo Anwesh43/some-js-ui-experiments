2123389e96b797ffba1bcf42746c12b5b9cfc294
anwesh43 <anweshthecool0@gmail.com>
2018-05-11 17:15:50 +0530

    adding TRLLAnimator class

--- TRotatingLinkedLineStage.js
+++ TRotatingLinkedLineStage.js
@@ -45,3 +45,25 @@ class TRLLState {
         }
     }
 }
+
+class TRLLAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
