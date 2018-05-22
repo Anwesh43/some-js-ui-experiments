de4e9b95798e87ce932a3848716d6fa7f24cc1d9
anwesh43 <anweshthecool0@gmail.com>
2018-05-22 15:43:33 +0530

    adding TranslateShrinkingCircleStage class

---
+++ TranslatingShrinkingCircleStage.js
@@ -0,0 +1,16 @@
+class TranslatingShrinkingCircleStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = (event) => {
+
+        }
+    }
+}
