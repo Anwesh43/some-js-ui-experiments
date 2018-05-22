179c807a6462908220290b54b5603009020abe7b
anwesh43 <anweshthecool0@gmail.com>
2018-05-22 15:58:23 +0530

    adding scale points

--- TranslatingShrinkingCircleStage.js
+++ TranslatingShrinkingCircleStage.js
@@ -17,18 +17,23 @@ class TranslatingShrinkingCircleStage extends CanvasStage {
 
 class TSCState {
     constructor() {
-        this.scale = 0
+        this.scales = [0, 0]
         this.dir = 0
         this.prevScale = 0
+        this.j = 0
     }
 
     update(stopcb) {
-        this.scale += 0.1 * this.dir
-        if (Math.abs(this.scale - this.prevScale) > 1) {
-            this.scale = this.prevScale + this.dir
-            this.dir = 0
-            this.prevScale = this.scale
-            stopcb()
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
         }
     }
 
