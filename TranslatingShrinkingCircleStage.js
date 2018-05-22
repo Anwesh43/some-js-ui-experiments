a2097d53ab1b4a4a0db127acb64c639d48fcb3c6
anwesh43 <anweshthecool0@gmail.com>
2018-05-22 16:42:44 +0530

    adding TSCNode class

--- TranslatingShrinkingCircleStage.js
+++ TranslatingShrinkingCircleStage.js
@@ -1,3 +1,5 @@
+const TSC_NODES = 5
+
 class TranslatingShrinkingCircleStage extends CanvasStage {
 
     constructor() {
@@ -65,5 +67,55 @@ class TSCAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+
+class TSCNode {
+
+    constructor(i) {
+        this.state = new TSCState()
+        if (i) {
+            this.i = i
+        }
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < TCS_NODES - 1) {
+            this.next = new TSCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+        const gap = (w/TCS_NODES)
+        context.fillStyle = '#c0392b'
+        context.save()
+        context.translate(this.i * gap, h/2)
+        context.beginPath()
+        context.arc(gap * this.state.scales[1], 0, (gap/10) * (1 - this.state.scales[1]) * (this.state.scales[0]), 0, 2 * Math.PI)
+        context.fill()
+        context.restore()
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (this.dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
