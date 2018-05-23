0e1c923234681a21fc23000bf47633ecfecf11ac
anwesh43 <anweshthecool0@gmail.com>
2018-05-23 15:30:27 +0530

    adding LABAnimator class

--- LinkedAnimBarStage.js
+++ LinkedAnimBarStage.js
@@ -40,3 +40,26 @@ class LABState {
         }
     }
 }
+
+class LABAnimator {
+
+    constructor() {
+        this.animated = false
+    }
+
+    start(updatecb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
