f70eb1b046e6d57f5416f761b121c5186ebf5324
anwesh43 <anweshthecool0@gmail.com>
2018-05-24 13:49:43 +0530

    adding QCNode class

--- QuarterCircleStage.js
+++ QuarterCircleStage.js
@@ -1,3 +1,4 @@
+const QC_NODES = 5
 class QuarterCircleStage extends CanvasStage {
 
     constructor() {
@@ -66,5 +67,46 @@ class QCAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class QCNode {
+    constructor(i) {
+        this.state = new QCState()
+        this.i = i
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < QC_NODES - 1) {
+            this.next = new QCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w , h) {
+        const r = Math.min(w, h)/(2 * QC_NODES)
+        context.save()
+        context.translate(this.i * r, this.i * r)
+        context.beginPath()
+        const start = 90 * this.state.scales[1], end = 90 * this.state.scales[0]
+        for(var i = start; i <= end; i++) {
+            const x = r * Math.cos(i * Math.PI/180), y = r * Math.sin(i * Math.PI/180)
+            if (i == start) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+        context.restore()
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
     }
 }
