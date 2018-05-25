406819a6cf8d98e38856888d2c3f825a9f9d166e
anwesh43 <anweshthecool0@gmail.com>
2018-05-25 13:56:17 +0530

    adding Error404Stage class

---
+++ 404ErrorStage.js
@@ -0,0 +1,16 @@
+class Error404Stage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
