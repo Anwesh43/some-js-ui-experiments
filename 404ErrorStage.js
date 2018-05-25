835a58e4607882883bcbf3d2a0c8c4b4e8f7f7ea
anwesh43 <anweshthecool0@gmail.com>
2018-05-25 14:13:38 +0530

    adding E404State class

--- 404ErrorStage.js
+++ 404ErrorStage.js
@@ -14,3 +14,29 @@ class Error404Stage extends CanvasStage {
         }
     }
 }
+
+class E404State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+        stopcb()
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
