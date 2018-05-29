28d733962362b25a6deab92bcf6b123bcbc33d29
anwesh43 <anweshthecool0@gmail.com>
2018-05-29 12:52:13 +0530

    adding SBFState class

--- SquareBlockFillerStage.js
+++ SquareBlockFillerStage.js
@@ -10,7 +10,33 @@ class SquareBlockFillerStage extends CanvasStage {
 
     handleTap() {
         this.canvas.onmousedown = (event) => {
-          
+
+        }
+    }
+}
+
+class SBFState {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
         }
     }
 }
