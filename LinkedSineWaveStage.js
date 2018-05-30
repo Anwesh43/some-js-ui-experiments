e6f35897a6ee54b40f077be5ede5e5b9951ba137
anwesh43 <anweshthecool0@gmail.com>
2018-05-30 12:31:15 +0530

    adding LSWState class

--- LinkedSineWaveStage.js
+++ LinkedSineWaveStage.js
@@ -14,3 +14,29 @@ class LinkedSineWaveStage extends CanvasStage {
         }
     }
 }
+
+class LSWState {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
