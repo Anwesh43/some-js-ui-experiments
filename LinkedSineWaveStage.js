9771b5727454ad0505949eb093b0c36aba8c5fa8
anwesh43 <anweshthecool0@gmail.com>
2018-05-30 12:33:23 +0530

    adding LSWAnimator class

--- LinkedSineWaveStage.js
+++ LinkedSineWaveStage.js
@@ -40,3 +40,25 @@ class LSWState {
         }
     }
 }
+
+class LSWAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
