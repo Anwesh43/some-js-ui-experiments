9c0f6639d168d4fecd5e173b74653f20f4580323
anwesh43 <anweshthecool0@gmail.com>
2018-06-06 14:21:08 +0530

    adding LSBAnimator class

--- LinkedStepBarStage.js
+++ LinkedStepBarStage.js
@@ -43,3 +43,26 @@ class LSBState {
         }
     }
 }
+
+class LSBAnimator {
+
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+      if (this.animated) {
+          this.animated = false
+          clearInterval(this.interval)
+      }
+    }
+}
