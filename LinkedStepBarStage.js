9432de2c5d833428ff6c79be1cd4fe25f7ffc384
anwesh43 <anweshthecool0@gmail.com>
2018-06-06 13:54:33 +0530

    adding LinkedStepBarStage class

---
+++ LinkedStepBarStage.js
@@ -0,0 +1,15 @@
+class LinkedStepBarStage extends CanvasStage{
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
