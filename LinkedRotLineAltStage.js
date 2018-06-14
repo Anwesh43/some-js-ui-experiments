59a5e94535b704347ae2f9d2e7b0fbf391ac1b2f
anwesh43 <anweshthecool0@gmail.com>
2018-06-14 16:07:46 +0530

    adding LRLNode class

--- LinkedRotLineAltStage.js
+++ LinkedRotLineAltStage.js
@@ -1,3 +1,4 @@
+const LRL_NODES = 5
 class LinkedRotLineAltStage extends CanvasStage {
 
     constructor() {
@@ -64,5 +65,58 @@ class LRLAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class LRLNode {
+    constructor(i) {
+        this.i = i
+        this.state = new LRLState()
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < LRL_NODES - 1) {
+            this.next = new LRLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+        const gap = ((w * 0.9) / LRL_NODES)
+        context.strokeStyle = '#27ae60'
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        if (this.prev) {
+            this.prev.draw(context, w, h)
+        }
+        const index = 1 - 2 * (i % 2)
+        context.save()
+        context.translate(this.i * gap + w/20 + gap * this.state.scales[1], h/2)
+        context.rotate(-Math.PI * index + Math.PI * this.state.scales[0] * index)
+        context.moveTo(0, 0)
+        context.lineTo(0, gap)
+        context.stroke()
+        context.restore()
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
     }
 }
