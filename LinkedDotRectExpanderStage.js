e2944e7c635ec8d2e06fbcc89be049ac2e55752e
anwesh43 <anweshthecool0@gmail.com>
2018-06-15 12:12:37 +0530

    adding LDRState class

--- LinkedDotRectExpanderStage.js
+++ LinkedDotRectExpanderStage.js
@@ -10,7 +10,35 @@ class LinkedDotRectExpanderStage extends CanvasStage {
 
     handleTap() {
         this.canvas.onmousedown = () => {
-            
+
+        }
+    }
+}
+
+class LDRState {
+    constructor() {
+        this.deg = 0
+        this.dir = 0
+        this.prevDeg = 0
+        this.scale = 0
+    }
+
+    update(stopcb) {
+        this.deg += Math.PI/10
+        this.scale = Math.sin(this.deg)
+        if (Math.abs(this.deg - this.prevDeg) > Math.PI/2) {
+            this.deg = this.prevDeg + this.dir * this.Math.PI/2
+            this.scale = Math.sin(this.deg)
+            this.dir = 0
+            this.prevDeg = 0
+            stopcb)
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * Math.floor(prevDeg / (Math.PI/2))
+            startcb()
         }
     }
 }
