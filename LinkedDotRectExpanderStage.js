b2d07dc4ec961532249c60e653c74766ea0a9363
anwesh43 <anweshthecool0@gmail.com>
2018-06-15 12:08:41 +0530

    adding LinkedDotRectExpanderStage class

---
+++ LinkedDotRectExpanderStage.js
@@ -0,0 +1,16 @@
+class LinkedDotRectExpanderStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            
+        }
+    }
+}
