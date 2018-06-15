34d5c22a742f77025fc76eccad3bd4e98f84d985
anwesh43 <anweshthecool0@gmail.com>
2018-06-15 12:14:19 +0530

    adding LDRAnimator class

--- LinkedDotRectExpanderStage.js
+++ LinkedDotRectExpanderStage.js
@@ -42,3 +42,25 @@ class LDRState {
         }
     }
 }
+
+class LDRAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
