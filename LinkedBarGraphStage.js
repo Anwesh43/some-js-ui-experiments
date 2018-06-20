bd5509d165f74ac608c3b987c0bae24e68913548
anwesh43 <anweshthecool0@gmail.com>
2018-06-20 14:09:56 +0530

    adding LBGState class

--- LinkedBarGraphStage.js
+++ LinkedBarGraphStage.js
@@ -1,3 +1,4 @@
+const LBG_NODES = 5
 class LinkedBarGraphStage extends CanvasStage {
     constructor() {
         super()
@@ -10,6 +11,31 @@ class LinkedBarGraphStage extends CanvasStage {
     handleTap() {
         this.canvas.onmousedown = (event) => {
 
+        }
+    }
+}
+
+class LBGState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
         }
     }
 }
