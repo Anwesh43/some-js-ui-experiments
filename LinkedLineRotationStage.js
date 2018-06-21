5c4dc63e78ff6b5624102c6ec333069191f57ef7
anwesh43 <anweshthecool0@gmail.com>
2018-06-21 13:02:01 +0530

    adding LRState class

--- LinkedLineRotationStage.js
+++ LinkedLineRotationStage.js
@@ -14,5 +14,27 @@ class LinkedLineRotationStage extends CanvasStage {
 
         }
     }
+  }
 
+  class LRState {
+      constructor() {
+          this.scale = 0
+          this.dir = 0
+          this.prevScale = 0
+      }
+
+      update(stopcb) {
+          this.scale += this.dir * 0.1
+          if (Math.abs(this.scale - this.prevScale) > 1) {
+              this.scale = this.prevScale + this.dir
+              this.dir = 0
+          }
+      }
+
+      startUpdating(startcb) {
+          if (this.dir == 0) {
+              this.dir = 1 - 2 * this.prevScale
+              startcb()
+          }
+      }
   }
