dbaecf200d50d7565babae9667ddacd228718880
anwesh43 <anweshthecool0@gmail.com>
2018-06-21 12:28:58 +0530

    adding LinkedLineRotationStage class

---
+++ LinkedLineRotationStage.js
@@ -0,0 +1,18 @@
+const LR_NODES = 5
+
+class LinkedLineRotationStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+  }
