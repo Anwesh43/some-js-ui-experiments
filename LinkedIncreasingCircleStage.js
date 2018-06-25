728c2b2d7631defe39f5d23600d78141b405d26e
anwesh43 <anweshthecool0@gmail.com>
2018-06-25 14:07:00 +0530

    adding LinkedIncreasingCircleStage class

---
+++ LinkedIncreasingCircleStage.js
@@ -0,0 +1,16 @@
+class LinkedIncreasingCircleStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
