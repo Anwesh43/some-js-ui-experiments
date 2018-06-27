b539e53fb8bdfb9091970252b2370803c6361265
anwesh43 <anweshthecool0@gmail.com>
2018-06-27 18:25:01 +0530

    adding LinkedMultiCircleStage class

---
+++ LinkedMultiCircleStage.js
@@ -0,0 +1,15 @@
+class LinkedMultiCircleStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = (event) => {
+
+        }
+    }
+}
