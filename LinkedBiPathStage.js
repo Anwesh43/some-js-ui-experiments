1802b30f0eb3661a6819c7827a6bbe480a3e7f68
anwesh43 <anweshthecool0@gmail.com>
2018-06-29 09:34:35 +0530

    adding LBPNode class

--- LinkedBiPathStage.js
+++ LinkedBiPathStage.js
@@ -9,7 +9,37 @@ class LinkedBiPathStage extends CanvasStage {
 
     handleTap() {
         this.canvas.onmousedown = () => {
-            
+
+        }
+    }
+}
+
+class LBPState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.j = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scales[this.j] += this.dir * 0.1
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
         }
     }
 }
