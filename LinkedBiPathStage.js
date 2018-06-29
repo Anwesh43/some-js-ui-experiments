0b512d05212854bcd11244849f5dac7cb41d0154
anwesh43 <anweshthecool0@gmail.com>
2018-06-29 09:29:45 +0530

    adding LinkedBiPathStage class

---
+++ LinkedBiPathStage.js
@@ -0,0 +1,15 @@
+class LinkedBiPathStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            
+        }
+    }
+}
