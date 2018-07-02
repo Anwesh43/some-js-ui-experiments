e763eaf393fa287e6b8035829493eba7a77a49d9
anwesh43 <anweshthecool0@gmail.com>
2018-07-02 12:08:22 +0530

    adding PyramidState class

--- LinkedPyramidStage.js
+++ LinkedPyramidStage.js
@@ -14,3 +14,30 @@ class LinkedPyramidStage extends CanvasStage {
         }
     }
 }
+
+class PyramidState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.prevScale = 0
+        this.j = 0
+    }
+
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.
+            }
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
