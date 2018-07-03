cccc2c5b1fd0d14634e4cd0d74706d8a0667c4ff
anwesh43 <anweshthecool0@gmail.com>
2018-07-03 16:05:27 +0530

    addinG SRState class

--- LinkedRectangleReducingStage.js
+++ LinkedRectangleReducingStage.js
@@ -1,3 +1,4 @@
+const SR_NODES = 5
 class LinkedRectangleReducingStage extends CanvasStage {
     constructor() {
         super()
@@ -10,6 +11,31 @@ class LinkedRectangleReducingStage extends CanvasStage {
     handleTap() {
         this.canvas.onmousedown = () => {
 
+        }
+    }
+}
+
+class SRState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
         }
     }
 }
