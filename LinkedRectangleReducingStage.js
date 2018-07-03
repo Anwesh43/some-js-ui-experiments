a95372431ef1120ef7df4e67f3affe1632db8b89
anwesh43 <anweshthecool0@gmail.com>
2018-07-03 17:39:11 +0530

    adding RRNode class

--- LinkedRectangleReducingStage.js
+++ LinkedRectangleReducingStage.js
@@ -1,4 +1,4 @@
-const SR_NODES = 5
+const RR_NODES = 10
 class LinkedRectangleReducingStage extends CanvasStage {
     constructor() {
         super()
@@ -59,5 +59,52 @@ class RRAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class RRNode {
+    constructor(i) {
+        this.i = i
+        this.state = new RRState()
+        this.addNeighbor()
+    }
+    addNeighbor() {
+        if (this.i < RR_NODES - 1) {
+            this.next = new RRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb) {
+        this.state.startUpdating(startcb)
+    }
+
+    draw(context, w, h) {
+        const gap = Math.min(w,h)/3 / RR_NODES
+        context.save()
+        context.translate(gap * this.i, h - gap/20 - this.i * context.strokeWidth)
+        context.beginPath()
+        context.moveTo((this.i) * gap * this.state.scale, 0)
+        context.lineTo((this.i + 1) * gap * this.state.scale, 0)
+        context.stroke()
+        context.restore()
+        if (this.next) {
+            this.next.draw(this.context, w, h)
+        }
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
