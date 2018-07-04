cb2e411b043d8ff990de5f3e6148b05d89b1d62a
anwesh43 <anweshthecool0@gmail.com>
2018-07-04 13:09:42 +0530

    adding UPArcAnimator class

--- LinkedUpArcStage.js
+++ LinkedUpArcStage.js
@@ -44,3 +44,25 @@ class UPArcState {
         }
     }
 }
+
+class UPArcAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
