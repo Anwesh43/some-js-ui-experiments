c64ca6792e67272d76ac55d4b15191d08af9613e
anwesh43 <anweshthecool0@gmail.com>
2018-07-04 12:56:48 +0530

    adding LinkedUpArcStage class

---
+++ LinkedUpArcStage.js
@@ -0,0 +1,17 @@
+const UP_ARC_NODES = 5
+
+class LinkedUpArcStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            
+        }
+    }
+}
