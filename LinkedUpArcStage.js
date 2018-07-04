eb1e55cf6d9793cc8edd0082b5560622ee70856a
anwesh43 <anweshthecool0@gmail.com>
2018-07-04 13:07:20 +0530

    adding UpArcState class

--- LinkedUpArcStage.js
+++ LinkedUpArcStage.js
@@ -11,7 +11,36 @@ class LinkedUpArcStage extends CanvasStage {
 
     handleTap() {
         this.canvas.onmousedown = () => {
-            
+
+        }
+    }
+}
+
+class UPArcState {
+    constructor() {
+        this.scales = [0, 0]
+        this.dir = 0
+        this.prevScale = 0
+        this.j = 0
+    }
+
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+            }
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
         }
     }
 }
