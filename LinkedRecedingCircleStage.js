97ad14b8ae1b8c51fb4eda0d9a68c5634d04a32c
anwesh43 <anweshthecool0@gmail.com>
2018-07-10 10:48:19 +0530

    adding LinkedRecedingCircleStage class

---
+++ LinkedRecedingCircleStage.js
@@ -0,0 +1,16 @@
+class LinkedRecedingCircleStage extends CanvasStage {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
