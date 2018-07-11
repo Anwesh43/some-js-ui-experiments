e82f497083976e4a6c69942fdf2f2d568be86b22
anwesh43 <anweshthecool0@gmail.com>
2018-07-11 13:32:15 +0530

    adding LinkedDecreasingRotatingLineStage class

---
+++ LinkedDecreasingRotatingLineStage.js
@@ -0,0 +1,15 @@
+const DRL_NODES = 5
+
+class LinkedDecreasingRotateingLineStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+
+    }
+}
