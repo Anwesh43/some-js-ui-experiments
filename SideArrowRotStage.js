98cb942699eb469f7a061b22733e3195cddd9212
anwesh43 <anweshthecool0@gmail.com>
2018-07-12 14:04:53 +0530

    adding SARState class

--- SideArrowRotStage.js
+++ SideArrowRotStage.js
@@ -1,3 +1,4 @@
+const SAR_NODES = 5
 class SideArrowRotStage extends CanvasStage {
     constructor() {
         super()
@@ -10,6 +11,30 @@ class SideArrowRotStage extends CanvasStage {
     handleTap() {
         this.canvas.onmousedown = () => {
 
+        }
+    }
+}
+
+class SARState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.prevScale - this.scale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
         }
     }
 }
