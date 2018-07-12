0d32fb1720d91813e2ba1d418278e067c23eb9f7
anwesh43 <anweshthecool0@gmail.com>
2018-07-12 14:06:48 +0530

    adding SARAnimator class

--- SideArrowRotStage.js
+++ SideArrowRotStage.js
@@ -38,3 +38,25 @@ class SARState {
         }
     }
 }
+
+class SARAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
