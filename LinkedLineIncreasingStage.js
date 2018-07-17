593a64ca1dfd7d687114ebf16e9e6ec27e206ce5
anwesh43 <anweshthecool0@gmail.com>
2018-07-17 15:09:20 +0530

    adding LinkedIncreasingLine class

--- LinkedLineIncreasingStage.js
+++ LinkedLineIncreasingStage.js
@@ -115,3 +115,28 @@ class LINode {
         return this
     }
 }
+
+class LinkedIncreasingLine {
+    constructor() {
+        this.root = new LInode(0)
+        this.curr = this.root
+        this.dir = 1
+    }
+
+    draw(context, w, h) {
+        this.root.draw(context, w, h)
+    }
+
+    update(stopcb) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            stopcb()
+        })
+    }
+
+    startUpdating(cb) {
+        this.curr.startUpdating(cb)
+    }
+}
