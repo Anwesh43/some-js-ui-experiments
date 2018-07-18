bcf73ec75e0b8b5a5f2e16c98e413df8f1b7fff0
anwesh43 <anweshthecool0@gmail.com>
2018-07-18 15:19:22 +0530

    adding BSMState class

--- BoxStepMoverStage.js
+++ BoxStepMoverStage.js
@@ -1,3 +1,4 @@
+const BSM_NODES = 5
 class BoxStepMoverStage extends CanvasStage {
     constructor() {
         super()
@@ -10,6 +11,31 @@ class BoxStepMoverStage extends CanvasStage {
     handleTap() {
         this.canvas.onmousedown = () => {
 
+        }
+    }
+}
+
+class BSMState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
         }
     }
 }
