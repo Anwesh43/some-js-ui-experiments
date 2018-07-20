bbbfc04e50e17f3896b5f937767ed64ce18d58a8
anwesh43 <anweshthecool0@gmail.com>
2018-07-20 09:50:55 +0530

    adding BrowserTabStage class

---
+++ BrowserTabStage.js
@@ -0,0 +1,16 @@
+const TAB_NODES = 5
+class BrowserTabStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
