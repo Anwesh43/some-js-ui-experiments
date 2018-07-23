6638ac83f61cb670a44b0432cc696b0d72ca18b4
anwesh43 <anweshthecool0@gmail.com>
2018-07-23 17:55:20 +0530

    adding LinkedPlusLineStage class

---
+++ LinkedPlusLineStage.js
@@ -0,0 +1,21 @@
+class LinkedPlusLineStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage = new LinkedPlusLineStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
