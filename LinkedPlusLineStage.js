433df84390af8c69639455dc2e56f8d367ceb23b
anwesh43 <anweshthecool0@gmail.com>
2018-07-23 17:58:31 +0530

    adding LPLState class

--- LinkedPlusLineStage.js
+++ LinkedPlusLineStage.js
@@ -19,3 +19,28 @@ class LinkedPlusLineStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class LPLState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
