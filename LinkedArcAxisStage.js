c87b7c612d6f270e024c6a57594c24c672ebd339
anwesh43 <anweshthecool0@gmail.com>
2018-07-24 15:09:39 +0530

    adding LAAAnimator class

--- LinkedArcAxisStage.js
+++ LinkedArcAxisStage.js
@@ -45,3 +45,25 @@ class LAAState {
         }
     }
 }
+
+class LAAAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start() {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
