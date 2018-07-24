ac6bc9d21326d34e75ff172ff7a23c4797519b84
anwesh43 <anweshthecool0@gmail.com>
2018-07-24 14:19:13 +0530

    adding LinkedArcAxisStage class

---
+++ LinkedArcAxisStage.js
@@ -0,0 +1,21 @@
+class LinkedArcAxisStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage = new LinkedArcAxisStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
