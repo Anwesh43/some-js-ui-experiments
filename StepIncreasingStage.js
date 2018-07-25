392074b172ced98755efb3ca03af49b789eee63f
anwesh43 <anweshthecool0@gmail.com>
2018-07-25 21:11:02 +0530

    adding SISState class

--- StepIncreasingStage.js
+++ StepIncreasingStage.js
@@ -19,3 +19,28 @@ class StepIncreasingStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class SISState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(stopcb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.prevScale - this.scale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
