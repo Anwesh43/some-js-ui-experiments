6bbaf31ac959de3aaf828169398fad3f2540f7ad
anwesh43 <anweshthecool0@gmail.com>
2018-07-27 11:50:39 +0530

    adding LADAnimator class

--- LinkedAxisDropBallStage.js
+++ LinkedAxisDropBallStage.js
@@ -44,3 +44,25 @@ class LADState {
         }
     }
 }
+
+class LADAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
