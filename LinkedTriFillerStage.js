e8fd08a16927563e6a5f0870c897c54454e80e25
anwesh43 <anweshthecool0@gmail.com>
2018-07-30 15:19:20 +0530

    adding TFAnimator class

--- LinkedTriFillerStage.js
+++ LinkedTriFillerStage.js
@@ -45,3 +45,25 @@ class TFState {
         }
     }
 }
+
+class TFAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
