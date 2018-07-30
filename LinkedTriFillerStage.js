6a2a9e8bab83a2cb356914a71304644f6e201ee7
anwesh43 <anweshthecool0@gmail.com>
2018-07-30 15:14:34 +0530

    adding TFState class

--- LinkedTriFillerStage.js
+++ LinkedTriFillerStage.js
@@ -20,3 +20,28 @@ class LinkedTriFillerStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class TFState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+
+    update(cb) {
+        this.scale += TF_SPEED * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+}
