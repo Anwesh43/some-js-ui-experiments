b2f3ac12915b569bc640f0edf148106bd6c9f4dc
anwesh43 <anweshthecool0@gmail.com>
2018-07-31 17:02:01 +0530

    adding SFSNode class

--- LinkedSquareFillStepStage.js
+++ LinkedSquareFillStepStage.js
@@ -63,3 +63,56 @@ class SFSAnimator {
         }
     }
 }
+
+class SFSNode {
+
+    constructor(i) {
+        this.i = i
+        this.state = new SFSState()
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < SFS_NODE - 1) {
+            this.next = new SFSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+        const sc1 = Math.min(this.state.scale, 0.5) * 2
+        const sc2 = Math.min(0.5, Math.max(this.state.scale - 0.5, 0)) * 2
+        const gap = (0.8 * w) / SFS_NODE
+        const size = gap / 2
+        context.save()
+        context.translate(gap * sc2)
+        context.save()
+        context.translate(0.1 * w + this.i * gap + gap / 2, h / 2)
+        context.rotate(i * Math.PI/2)
+        context.scale(sc1, sc1)
+        context.fillStyle = 'white'
+        context.fillRect(0, 0, size/2, size/2)
+        context.restore()
+        context.restore()
+    }
+
+    update(cb) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
