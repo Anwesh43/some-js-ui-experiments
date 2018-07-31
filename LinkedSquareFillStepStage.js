dfae080beaad1863be87b5d8116cb66ab3eeb5d8
anwesh43 <anweshthecool0@gmail.com>
2018-07-31 14:49:32 +0530

    adding SFSAnimator class

--- LinkedSquareFillStepStage.js
+++ LinkedSquareFillStepStage.js
@@ -41,3 +41,25 @@ class SFSState {
         }
     }
 }
+
+class SFSAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
