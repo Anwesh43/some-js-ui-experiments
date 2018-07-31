a02695cb0110952d057b443fa6f8a90845cdbf22
anwesh43 <anweshthecool0@gmail.com>
2018-07-31 14:46:40 +0530

    adding SFSState class

--- LinkedSquareFillStepStage.js
+++ LinkedSquareFillStepStage.js
@@ -1,3 +1,4 @@
+const SFS_SPEED = 0.05, SFS_NODE = 3
 class LinkedSquareFillStepStage extends CanvasStage {
     constructor() {
         super()
@@ -12,6 +13,31 @@ class LinkedSquareFillStepStage extends CanvasStage {
     }
 
     static init() {
-        
+
+    }
+}
+
+class SFSState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += SFS_SPEED * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
     }
 }
