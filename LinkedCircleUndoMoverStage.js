9e409bc215352ecb7e06498c6e5a87172d3111ce
anwesh43 <anweshthecool0@gmail.com>
2018-08-01 10:52:18 +0530

    adding LinkedCircleUndoMoverStage class

---
+++ LinkedCircleUndoMoverStage.js
@@ -0,0 +1,15 @@
+class LinkedCircleUndoMoverStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
