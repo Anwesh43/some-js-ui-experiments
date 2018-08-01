8773b611d41d7e20db3447d130dd649e3a9d16c3
anwesh43 <anweshthecool0@gmail.com>
2018-08-01 17:10:00 +0530

    adding LinkedSquareFiller class

--- LinkedSquareFillStepStage.js
+++ LinkedSquareFillStepStage.js
@@ -116,3 +116,27 @@ class SFSNode {
         return this
     }
 }
+
+class LinkedSquareFiller {
+    constructor() {
+        this.curr = new SFSNode(0)
+        this.dir = 1
+    }
+
+    draw(context, w, h) {
+        this.curr.draw(context, w, h)
+    }
+
+    update(cb) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb) {
+        this.curr.startUpdating(cb)
+    }
+}
