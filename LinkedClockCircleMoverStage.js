0eaa88f9c8721226e9e3da0599d3e35155ccca3b
anwesh43 <anweshthecool0@gmail.com>
2018-08-03 12:42:21 +0530

    adding CCMState class

--- LinkedClockCircleMoverStage.js
+++ LinkedClockCircleMoverStage.js
@@ -9,7 +9,32 @@ class LinkedClockCircleMoverStage extends CanvasStage {
 
     handleTap() {
         this.canvas.onmousedown = () => {
-            
+
+        }
+    }
+}
+
+class CCMState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+
+    update(cb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.prevScale - this.scale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
         }
     }
 }
