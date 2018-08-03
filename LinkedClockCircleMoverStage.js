8b9072d8f43df62b6e8e74bc8533591815f938d5
anwesh43 <anweshthecool0@gmail.com>
2018-08-03 12:43:53 +0530

    adding CCMAnimator class

--- LinkedClockCircleMoverStage.js
+++ LinkedClockCircleMoverStage.js
@@ -38,3 +38,25 @@ class CCMState {
         }
     }
 }
+
+class CCMAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                cb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
