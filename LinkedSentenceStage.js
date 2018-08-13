2acd031d8d19e1f33150bab4c5535e323445c399
anwesh43 <anweshthecool0@gmail.com>
2018-08-13 13:12:05 +0530

    adding LinkedSentenceStage class

---
+++ LinkedSentenceStage.js
@@ -0,0 +1,21 @@
+class LinkedSentenceStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+
+    render() {
+        super.render()
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage = new LinkedSentenceStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
