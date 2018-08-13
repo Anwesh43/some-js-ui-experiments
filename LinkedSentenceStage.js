e7d5dcf6982e944032a6bf9eab965c1127767ea6
anwesh43 <anweshthecool0@gmail.com>
2018-08-13 14:46:23 +0530

    adding LSSNode class

--- LinkedSentenceStage.js
+++ LinkedSentenceStage.js
@@ -65,3 +65,56 @@ class LSSAnimator {
         }
     }
 }
+
+class LSSNode {
+    constructor(i) {
+        this.i = i
+        this.state = new LSSState()
+        this.addNeighbor()
+    }
+
+    update(cb) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb) {
+        this.state.startUpdating(cb)
+    }
+
+    addNeighbor() {
+        const words = node_sentence.split(" ")
+        if (this.i < words.length) {
+            this.word = words[this.i]
+        }
+        if (this.i < words.length - 1) {
+            this.next = new LLSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+        const n = node_sentence.split(" ").length
+        const gap = w / n
+        context.font = context.font.replace(/\d{2}/, Math.min(w, h) / 20)
+        context.fillStyle = 'white'
+        const tw = context.measureText(this.word).width
+        context.save()
+        context.translate(this.i * gap + gap/2, h/2)
+        context.rotate(2 * Math.PI * this.state.scale)
+        context.scale(this.state.scale, this.state.scale)
+        context.fillText(this.word, -tw/2, Math.min(w, h) / 40)
+        context.restore()
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
