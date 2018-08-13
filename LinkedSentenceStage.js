c8d16909e533fa11f9c3309b79471d8cb56f6e1c
anwesh43 <anweshthecool0@gmail.com>
2018-08-13 13:14:27 +0530

    adding State class

--- LinkedSentenceStage.js
+++ LinkedSentenceStage.js
@@ -19,3 +19,28 @@ class LinkedSentenceStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class LSState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += 0.05 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
