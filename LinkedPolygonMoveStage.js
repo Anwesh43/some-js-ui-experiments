f67f85f9f6b88dbc33dbfeff4f4852322ff4661a
anwesh43 <anweshthecool0@gmail.com>
2018-08-21 16:42:09 +0530

    adding PMState class

--- LinkedPolygonMoveStage.js
+++ LinkedPolygonMoveStage.js
@@ -17,3 +17,28 @@ class LinkedPolygonMoveStage {
         stage.handleTap()
     }
 }
+
+class PMState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
