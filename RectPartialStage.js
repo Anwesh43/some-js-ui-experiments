a0878acbc1a215b70ec72b78d9062f18a60416f0
anwesh43 <anweshthecool0@gmail.com>
2018-08-22 16:45:16 +0530

    adding RPNode class

--- RectPartialStage.js
+++ RectPartialStage.js
@@ -64,3 +64,66 @@ class RPAnimator {
         }
     }
 }
+
+class RPNode {
+    constructor(i) {
+        this.i = i
+        this.state = new RPState()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RPNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        context.strokeStyle = '#FF9800'
+        const gap = w / (nodes + 1)
+        var sc1 = Math.min(0.5, this.state.scale) * 2
+        const sc2 = Math.min(0.5, this.state.scale - 0.5) * 2
+        const size = gap / 4
+        sc1 = (1 - sc1) * (this.i % 2) + (1 - (this.i % 2)) * sc1
+        context.save()
+        context.translate(gap * i + gap * sc2, h / 2)
+        for(var i = 0; i < 2; i++) {
+            context.save()
+            context.scale(1 - 2 * i, 1)
+            context.save()
+            context.translate(size/4 * sc1, -size/2)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(-size/4, 0)
+            context.lineTo(-size/4, size)
+            context.lineTo(0, size)
+            context.stroke()
+            context.restore()
+            context.restore()
+        }
+
+        context.restore()
+    }
+
+    update(cb) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
