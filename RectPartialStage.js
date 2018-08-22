6401c98d5ec66c092f368fca2ee51d26f6b8ffeb
anwesh43 <anweshthecool0@gmail.com>
2018-08-22 16:48:14 +0530

    adding LinkedRP class

--- RectPartialStage.js
+++ RectPartialStage.js
@@ -127,3 +127,27 @@ class RPNode {
         return this
     }
 }
+
+class RectPartial {
+    constructor() {
+        this.curr = new RPNode(0)
+        this.dir = 1
+    }
+
+    draw(context, w, h) {
+        this.curr.draw(context, w, h)
+    }
+
+    update(cb) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb) {
+        this.curr.startUpdating(cb)
+    }
+}
