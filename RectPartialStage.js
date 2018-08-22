321af8199e74f354d705182b52c6620edab06aef
anwesh43 <anweshthecool0@gmail.com>
2018-08-22 16:31:55 +0530

    adding RPAnimator class

--- RectPartialStage.js
+++ RectPartialStage.js
@@ -19,3 +19,48 @@ class RectPartialStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class RPState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
+
+class RPAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
