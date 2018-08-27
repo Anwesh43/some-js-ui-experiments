3d9ce7a5c56f291dce9682f267e1e76ebee18c16
anwesh43 <anweshthecool0@gmail.com>
2018-08-27 16:15:46 +0530

    adding TIState class

--- LinkedIncreasingTreeStage.js
+++ LinkedIncreasingTreeStage.js
@@ -20,3 +20,28 @@ class LinkedIncreasingTreeStage extends CanvasStage {
 		stage.handleTap()
 	}
 }
+
+class TIState {
+	constructor() {
+		this.scale = 0
+		this.dir = 0
+		this.prevScale = 0
+	}
+
+	update(cb) {
+		this.scale += this.dir * 0.1 
+		if (Math.abs(this.scale - this.prevScale) > 1) {
+			this.scale = this.prevScale + this.dir 
+			this.dir = 0
+			this.prevScale = this.scale 
+			cb()
+		}
+	}
+
+	startUpdating(cb) {
+		if (this.dir == 0) {
+			this.dir = 1 - 2 * this.prevScale 
+		}
+	}
+}
+
