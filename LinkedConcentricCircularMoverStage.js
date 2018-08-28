609f63b0ef0bc3869744d2442006147ca2e52da5
anwesh43 <anweshthecool0@gmail.com>
2018-08-28 15:00:16 +0530

    adding ConcCINode class

--- LinkedConcentricCircularMoverStage.js
+++ LinkedConcentricCircularMoverStage.js
@@ -1,4 +1,4 @@
-const CCM_NODES = 5
+const CONCCI_NODES = 5
 class LinkedConcentricCircularMoverStage extends CanvasStage {
 	constructor() {
 		super()
@@ -64,6 +64,64 @@ class ConcCiAnimator {
 			this.animated = false 
 			clearInterval(this.interval, 50)
 		}
+	}
+}
+
+class ConcCiNode {
+	constructor(i) {
+		this.i = i 	
+		this.state = new ConcCiState()
+		this.addNeighbor()
+	}
+
+	addNeighbor() {
+		if (this.i < CONCCI_NODES - 1) {
+			this.next = new ConcCiNode(this.i + 1)
+			this.next.prev = this 
+		}
+	}
+
+	draw(context, w, h, drawNext, drawPrev) {
+		context.lineWidth = Math.min(w, h) / 60
+		context.lineCap = 'round'
+		context.strokeStyle = 'white'
+		const gap = h / (CONCCI_NODES+1)
+		var r = (gap / (CONCCI_NODES)) * (CONCCI_NODES - this.i) 
+		context.save()
+		context.translate(w/2, (gap * this.i) + gap + gap * this.state.scale)
+		context.beginPath()
+		context.arc(0, 0, r, 0, 2 * Math.PI)
+		context.stroke()
+		if (this.prev && drawPrev) {
+			context.save()
+			context.translate(0, -gap)
+			this.prev.draw(context, w, h, false, true)
+			context.restore()	
+		}
+		if (this.next && drawNext) {
+			this.next.draw(context, w, h, true, false)
+		}
+		context.restore()
+	}
+
+	update(cb) {
+		this.state.update(cb)
+	}
+
+	startUpdating(cb) {
+		this.state.startUpdating(cb)
+	}
+
+	getNext(dir, cb) {
+		var curr = this.prev 
+		if (dir == 1) {
+			curr = this.next 
+		}
+		if (curr) {
+			return curr 
+		}
+		cb()
+		return this 
 	}
 }
 
