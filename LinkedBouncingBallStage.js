9762ac95b847a185110b0bb3f5c20c77252925d0
anwesh43 <anweshthecool0@gmail.com>
2018-09-04 19:54:24 +0530

    adding B2State class

--- LinkedBouncingBallStage.js
+++ LinkedBouncingBallStage.js
@@ -19,3 +19,28 @@ class LinkedBouncingBallStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class B2State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += 0.05 * this.prevScale
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
