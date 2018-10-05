f6ce170b3ccc5433d17da3613166b18d5cc1479e
anwesh43 <anweshthecool0@gmail.com>
2018-10-05 13:07:38 +0530

    adding XCSState class

--- LinkedXCreateStepStage.js
+++ LinkedXCreateStepStage.js
@@ -1,3 +1,4 @@
+const XCS_NODE = 5, XCS_STEP  = 2
 class LinkedXCreateStepStage extends CanvasStage {
     constructor() {
         super()
@@ -17,5 +18,31 @@ class LinkedXCreateStepStage extends CanvasStage {
         const stage = new LinkedXCreateStepStage()
         stage.render()
         stage.handleTap()
+    }
+}
+
+
+class XCSState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+
+    update(cb) {
+        this.scale += (0.1 / XCS_STEP) * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
     }
 }
