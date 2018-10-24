1375545f6186b84585be7fa97ee0bdee46287af1
anwesh43 <anweshthecool0@gmail.com>
2018-10-24 09:35:40 +0530

    adding RSState class

--- LinkedRectShifterStage.js
+++ LinkedRectShifterStage.js
@@ -21,3 +21,28 @@ class LinkiedRectShifterStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class RSState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += (0.1 / RS_STEPS) * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
