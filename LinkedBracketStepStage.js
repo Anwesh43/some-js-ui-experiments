9248982b6514a7c40f66a98dcda2de0638d4be97
anwesh43 <anweshthecool0@gmail.com>
2018-11-02 16:43:56 +0530

    adding LBSSNode class

--- LinkedBracketStepStage.js
+++ LinkedBracketStepStage.js
@@ -1,3 +1,25 @@
+const LBSS_NODES = 5
+const LBSS_SIDES = 3
+const LBSS_FACTOR = 2
+
+const LBSS_divideScale = (scale, i, n) => Math.min((1/n), Math.max(0, scale - (1/n) * i))
+
+const LBSS_drawLines = [(context, size, sc) => {
+    context.beginPath()
+    context.moveTo(0, 0)
+    context.lineTo((size) * sc, 0)
+    context.stroke()
+}, (context, size, sc) => {
+    context.beginPath()
+    context.moveTo(size, 0)
+    context.moveTo(size, 2 * size * sc)
+    context.stroke()
+}, (context, size, sc) => {
+    context.beginPath()
+    context.moveTo(size, 2 * size)
+    context.lineTo(size - size * sc, 2 * size)
+    context.stroke()
+}]
 class LinkedBrackedStepStage extends CanvasStage {
     constructor() {
         super()
@@ -56,5 +78,63 @@ class LBSSAnimator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class LBSSNode {
+    constructor(i) {
+        this.i = i
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < LBSS_NODES) {
+            this.next = new LBSSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context, w, h) {
+        const gap = w / (nodes + 1)
+        context.lineWidth = Math.min(w, h) / 60
+        context.lineCap = 'round'
+        context.strokeStyle = '#1A237E'
+        const size = gap / 3
+        context.save()
+        context.translate(this.i * gap + gap, h/2)
+        for (var i = 0; i < LBSS_FACTOR; i++) {
+            const sc = LBSS_divideScale(this.state.scale, i, LBSS_FACTOR)
+            context.save()
+            context.scale(1 - 2 * i, 1)
+            for(var j = 0; j < LBSS_SIDES; j++) {
+                context.save()
+                context.translate(0, -size)
+                const scj = LBSS_divideScale(sc, j, LBSS_SIDES)
+                LBSS_drawLines(context, size, scj)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir, cb) {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
