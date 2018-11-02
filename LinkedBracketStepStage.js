8d13b0e73d2cb64da4d7480e19336f1276b67e4b
anwesh43 <anweshthecool0@gmail.com>
2018-11-02 15:41:43 +0530

    adding LBSSAnimator class

--- LinkedBracketStepStage.js
+++ LinkedBracketStepStage.js
@@ -38,3 +38,23 @@ class LBSState {
         }
     }
 }
+
+class LBSSAnimator {
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
