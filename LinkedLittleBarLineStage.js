3ad307c43cdcbb7aac0cf6c1eae59f2746f921d7
anwesh43 <anweshthecool0@gmail.com>
2018-12-05 13:46:14 +0530

    adding number methods and constants

--- LinkedLittleBarLineStage.js
+++ LinkedLittleBarLineStage.js
@@ -1,3 +1,16 @@
+const LLBL_NODES = 5
+const LLBL_BARS = 4
+const LLBL_scDiv = 0.51
+const LLBL_scGap = 0.05
+
+const LLBL_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, scale - i/n)) * n
+const LLBL_scaleFactor = (scale) => Math.floor(scale / LLBL_scDiv)
+const LLBL_mirrorValue = (scale, a, b) => {
+    const k = LLBL_scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const LLBL_updateScale = (scale, dir, a, b) => dir * LLBL_scGap * LLBL_mirrorValue(scale, a, b)
+
 class LinkedLittleBarLineStage extends CanvasStage {
     constructor() {
         super()
