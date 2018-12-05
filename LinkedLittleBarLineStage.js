8779f0bb841db63de73f55ad9b20f7f5f2a0a637
anwesh43 <anweshthecool0@gmail.com>
2018-12-05 14:08:40 +0530

    adding drawLLBLNode method

--- LinkedLittleBarLineStage.js
+++ LinkedLittleBarLineStage.js
@@ -2,6 +2,7 @@ const LLBL_NODES = 5
 const LLBL_BARS = 4
 const LLBL_scDiv = 0.51
 const LLBL_scGap = 0.05
+const LLBL_sizeFactor = 2.1
 
 const LLBL_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, scale - i/n)) * n
 const LLBL_scaleFactor = (scale) => Math.floor(scale / LLBL_scDiv)
@@ -10,6 +11,39 @@ const LLBL_mirrorValue = (scale, a, b) => {
     return (1 - k) / a + k / b
 }
 const LLBL_updateScale = (scale, dir, a, b) => dir * LLBL_scGap * LLBL_mirrorValue(scale, a, b)
+
+const drawLLBLNode = (context, i, scale, w, h) => {
+    const gap = w / LLBL_NODES
+    const size = gap / LLBL_sizeFactor
+    const sc1 = LLBL_divideScale(scale, 0, 2)
+    const sc2 = LLBL_divideScale(scale, 1, 2)
+    context.save()
+    context.translate((i + 1) * gap, h/2)
+    context.rotate(Math.PI/2 * sc2)
+    const n = Math.sqrt(LLBL_BARS)
+    for (var j = 0; j < n; j++) {
+        context.save()
+        context.scale((1 - 2 * j), 1)
+        context.translate(size, 0)
+        for (var p = 0; p < n; p++) {
+            const k = j * n + p
+            const sc = LLBL_divideScale(sc1, k, LLBL_BARS)
+            context.save()
+            context.translate(0, size * (1 - 2 * p) * sc)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, -size/4)
+            context.stroke()
+            context.restore()
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, size * (1 - 2 * p) * sc)
+            context.stroke()
+        }
+        context.restore()
+    }
+    context.restore()
+}
 
 class LinkedLittleBarLineStage extends CanvasStage {
     constructor() {
