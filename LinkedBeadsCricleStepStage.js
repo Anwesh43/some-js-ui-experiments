85bf23a261b47e53444f49e565e8005deba96e0d
anwesh43 <anweshthecool0@gmail.com>
2018-12-06 15:13:39 +0530

    adding float methods

--- LinkedBeadsCricleStepStage.js
+++ LinkedBeadsCricleStepStage.js
@@ -3,6 +3,18 @@ const BCS_beads = 6
 const BCS_scDiv = 0.51
 const BCS_scGap = 0.05
 const BCS_color = "#1A237E"
+
+const BCS_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, scale - i / n)) * n
+
+const BCS_scaleFactor = (scale) => Math.floor(scale / BCS_scDiv)
+
+const BCS_mirrorValue = (scale, a, b) => {
+    const k = BCS_scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const BCS_updateScale = (scale, dir, a, b) => dir * BCS_scGap * BCS_mirrorValue(scale, a, b)
+
 class LinkedBeadsCircleStepStage extends CanvasStage {
     constructor() {
         super()
