4d756f089c0a76a07a4290ab267d6b29a1fbcfbb
anwesh43 <anweshthecool0@gmail.com>
2018-12-06 15:32:27 +0530

    adding BCSState class

--- LinkedBeadsCricleStepStage.js
+++ LinkedBeadsCricleStepStage.js
@@ -70,3 +70,28 @@ class LinkedBeadsCricleStepStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class BCSState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += BCS_updateScale(this.scale, this.dir, BCS_beads, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
