e98d5f10d1154e6cdacd565db220c34dda41838b
anwesh43 <anweshthecool0@gmail.com>
2018-12-07 15:48:56 +0530

    adding drawLIRBNode method

--- LinkedLineInRectBarStage.js
+++ LinkedLineInRectBarStage.js
@@ -3,6 +3,8 @@ const LIRB_LINES = 4
 const LIRB_scDiv = 0.51
 const LIRB_scGap = 0.05
 const LIRB_color = "#0D47A1"
+const LIRB_sizeFactor = 2.2
+const LIRB_strokeFactor = 90
 
 const LIRB_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, scale - i / n)) * n
 const LIRB_scaleFactor = (scale) => Math.floor(scale / LIRB_scDiv)
@@ -11,6 +13,40 @@ const LIRB_mirrorValue = (scale, a, b) => {
     return ((1 - k) / a) + (k / b)
 }
 const LIRB_updateScale = (scale, dir, a, b) => LIRB_scaleFactor(scale, a, b) * dir * LIRB_scGap
+
+const drawLIRBNode = (context, i, scale, w, h) => {
+    const gap = w / LIRB_NODES
+    const size = gap / LIRB_sizeFactor
+    context.lineWidth = Math.min(w, h) / LIRB_strokeFactor
+    context.lineCap = 'round'
+    context.strokeStyle = LIRB_color
+    context.fillStyle = '#BDBDBD'
+    const sc1 = LIRB_divideScale(scale, 0, 2)
+    const sc2 = LIRB_divideScale(scale, 1, 2)
+    context.save()
+    context.translate(gap * (this.i + 1), h/2)
+    for (var j = 0; j < 2; j++) {
+        const sck = LIRB_divideScale(sc2, j, 2)
+        context.save()
+        context.rotate(Math.PI/2 * sc1 * j)
+        context.fillRect(-size, -size/4, size, size/2)
+        const ox = -size + size * j
+        const xGap = size / (LIRB_LINES + 1)
+        const sf = 1 - 2 * j
+        for (var k = 0; k < lines; k++) {
+            const scj = LIRB_divideScale(sck, k, LIRB_LINES)
+            context.save()
+            context.translate(ox + xGap * k * sf, -size/4)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, size/2 * scj)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    context.restore()
+}
 
 class LinkedLineInRectBarStage extends CanvasStage {
     constructor() {
