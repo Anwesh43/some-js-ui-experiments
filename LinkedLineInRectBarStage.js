1e0a6eefab8e42b8eac8615b7fc33fd58336ce27
anwesh43 <anweshthecool0@gmail.com>
2018-12-07 15:07:19 +0530

    adding LIRB number methods

--- LinkedLineInRectBarStage.js
+++ LinkedLineInRectBarStage.js
@@ -4,6 +4,14 @@ const LIRB_scDiv = 0.51
 const LIRB_scGap = 0.05
 const LIRB_color = "#0D47A1"
 
+const LIRB_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, scale - i / n)) * n
+const LIRB_scaleFactor = (scale) => Math.floor(scale / LIRB_scDiv)
+const LIRB_mirrorValue = (scale, a, b) => {
+    const k = LIRB_scaleFactor(scale)
+    return ((1 - k) / a) + (k / b)
+}
+const LIRB_updateScale = (scale, dir, a, b) => LIRB_scaleFactor(scale, a, b) * dir * LIRB_scGap
+
 class LinkedLineInRectBarStage extends CanvasStage {
     constructor() {
         super()
