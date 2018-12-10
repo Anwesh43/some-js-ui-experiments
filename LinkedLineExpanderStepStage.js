53e1ca1492ebc36ad546ca2fda576b3fcfed93a0
anwesh43 <anweshthecool0@gmail.com>
2018-12-10 15:18:01 +0530

    adding scale functions

--- LinkedLineExpanderStepStage.js
+++ LinkedLineExpanderStepStage.js
@@ -3,6 +3,19 @@ const LES_LINES = 2
 const LES_LINE_WIDTH = 60
 const LES_COLOR = "#0D47A1"
 const LES_SIZE_FACTOR = 90
+const LES_scGap = 0.05
+const LES_scDiv = 0.51
+
+const LES_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, i / n)) * n
+
+const LES_scaleFactor = (scale) =>  Math.floor(scale / LES_scDiv)
+
+const LES_mirrorValue = (scale, a, b) => {
+    const k = LES_scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const LES_updateScale = (scale, dir, a, b) => LES_mirrorValue(scale, a, b) * dir * LES_scGap
 
 class LinkedLineExpandingStepStage extends CanvasStage {
     constructor() {
