43a561a874f899accd1e92fa198871081f82eadc
anwesh43 <anweshthecool0@gmail.com>
2018-12-10 14:49:48 +0530

    adding LinkedLineExpanderStepStage class

---
+++ LinkedLineExpanderStepStage.js
@@ -0,0 +1,23 @@
+const LES_NODES = 5
+const LES_LINES = 2
+const LES_LINE_WIDTH = 60
+const LES_COLOR = "#0D47A1"
+const LES_SIZE_FACTOR = 90
+
+class LinkedLineExpandingStepStage extends CanvasStage {
+    constructor() {
+        super()
+    }
+    render() {
+
+    }
+    handleTap() {
+
+    }
+
+    static init() {
+        const stage = new LinkedLineExpandingStepStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
