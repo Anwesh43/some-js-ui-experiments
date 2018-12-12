a555b855e5850bf41289ddd8f0be1d8ea03de338
anwesh43 <anweshthecool0@gmail.com>
2018-12-12 16:06:48 +0530

    adding BUMNState class

--- BoxUpMoverStage.js
+++ BoxUpMoverStage.js
@@ -19,3 +19,28 @@ class BoxUpMoverStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class BUMNState {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
