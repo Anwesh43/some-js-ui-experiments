bd29cad996b0bef4d9a5698b2b741df696031192
anwesh43 <anweshthecool0@gmail.com>
2018-12-13 16:08:14 +0530

    adding fixes

--- LinkedBallBoxOpenStage.js
+++ LinkedBallBoxOpenStage.js
@@ -10,7 +10,7 @@ const BBO_divideScale = (scale, i, n) => {
     return Math.min(1/n, Math.max(0, scale - i/n)) * n
 }
 
-const BBO_scaleFactor = (scale) => Math.floor(this / BBO_scDiv)
+const BBO_scaleFactor = (scale) => Math.floor(scale / BBO_scDiv)
 
 const BBO_mirrorValue = (scale, a, b) => {
     const k = BBO_scaleFactor(scale)
@@ -20,33 +20,35 @@ const BBO_mirrorValue = (scale, a, b) => {
 const BBO_updateScale = (scale, dir, a, b) => BBO_mirrorValue(scale, a, b) * dir * BBO_scGap
 
 const drawBBONode = (context, i, scale, w, h) => {
-    const gap = h / (nodes + 1)
+    const gap = h / (BBO_NODES + 1)
     const size = gap / BBO_sizeFactor
     context.strokeStyle = BBO_color
     context.fillStyle = BBO_color
     context.lineWidth = Math.min(w, h) / BBO_strokeFactor
     context.lineCap = 'round'
+    const r = size/2
     context.save()
     context.translate(w/2, gap * (i + 1))
     for (var j = 0; j < BBO_BALLS; j++) {
         const sc = BBO_divideScale(scale, j, BBO_BALLS)
         const sc1 = BBO_divideScale(sc, 0, BBO_BALLS)
         const sc2 = BBO_divideScale(sc, 1, BBO_BALLS)
+        console.log(`${sc1},${sc2}`)
         context.save()
         context.scale(1 - 2 * j, 1 - 2 * j)
         context.save()
-        context.translate((w/2 - size) * sc2, 0)
+        context.translate((w/2 - r) * sc2, 0)
         context.beginPath()
-        context.arc(0, 0, size, 0, 2 * Math.PI)
+        context.arc(0, 0, r, 0, 2 * Math.PI)
         context.fill()
         context.restore()
         for(var k = 0; k < 2; k++) {
             context.save()
             context.translate(size, -size)
-            context.rotate((Math.PI/2 + Math.PI/2 * sc1) * k)
+            context.rotate(-(Math.PI/2 + Math.PI/2 * sc1) * k)
             context.beginPath()
             context.moveTo(0,0)
-            context.lineTo(-size, 0)
+            context.lineTo(-2 * size, 0)
             context.stroke()
             context.restore()
         }
@@ -92,8 +94,8 @@ class BBOState {
     }
 
     update(cb) {
-        this.scale += BBO_updateScale(this.scale, this.dir, BBO_LINES, BBO_LINES)
-        if (Math.abs(this.scale - this.prevScale) > 0) {
+        this.scale += BBO_updateScale(this.scale, this.dir, BBO_BALLS, BBO_BALLS)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir
             this.dir = 0
             this.prevScale = this.scale
@@ -187,6 +189,7 @@ class BallBoxOpen {
             this.curr = this.curr.getNext(this.dir, () => {
                 this.dir *= -1
             })
+            cb()
         })
     }
 
