86bdacc51249bd3d6d388a126904fa072327c3a5
anwesh43 <anweshthecool0@gmail.com>
2018-12-19 13:08:25 +0530

    adding drawCAENode method

--- LinkedCircleArcExpanderStage.js
+++ LinkedCircleArcExpanderStage.js
@@ -5,6 +5,7 @@ const CAE_sizeFactor = 3
 const CAE_color = "#311B92"
 const CAE_scDiv = 0.51
 const CAE_scGap = 0.05
+const CAE_point = 15
 
 const CAE_divideScale = (scale, i, n) => Math.min(1/n, Math.max(0, scale - i / n)) * n
 
@@ -13,3 +14,41 @@ const CAE_scaleFactor = (scale) => Math.floor(scale / CAE_scDiv)
 const CAE_mirrorValue = (scale, a, b) => (1 - CAE_scaleFactor(scale)) / a + CAE_scaleFactor(scale) / b
 
 const CAE_updateScale = (scale, dir, a, b) => CAE_mirrorValue(scale, a, b) * dir * CAE_scGap
+
+const drawCAENode = (context, i, scale, w, h) => {
+    const gap = w / (CAE_nodes + 1)
+    const sc1 = CAE_divideScale(scale, 0, 2)
+    const sc2 = CAE_divideScale(scale, 1, 2)
+    context.lineWidth = Math.min(w, h) / CAE_strokeFactor
+    context.lineCap = 'round'
+    context.strokeStyle = CAE_color
+    const size = gap / CAE_sizeFactor
+    const r = size / 2
+    context.save()
+    context.translate(gap * (i + 1), h/2)
+    context.rotate(Math.PI/2 * sc2)
+    for (var j = 0; j < CAE_arcs; j++) {
+        const sc = CAR_divideScale(scale, j, CAE_arcs)
+        context.save()
+        context.rotate(Math.PI/2 * j)
+        for (k = 0; k < 2; k++) {
+            context.save()
+            context.scale(1, 1 - 2 * k)
+            context.beginPath()
+            for (var t = -45; t <= 0; t++) {
+                var a = r * Math.max((t+CAE_point)/CAE_point, 0) * sc
+                const x = (r + a) * Math.cos(t * Math.PI/180)
+                const y = (r + a) * Math.sin(t * Math.PI/180)
+                if (t == -45) {
+                    context.moveTo(x, y)
+                } else {
+                    context.lineTo(x, y)
+                }
+            }
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+    context.restore()
+}
