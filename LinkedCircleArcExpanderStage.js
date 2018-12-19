26d10bfe9526dde705247a904d4e38e8db328843
anwesh43 <anweshthecool0@gmail.com>
2018-12-19 14:47:04 +0530

    adding CAEState class

--- LinkedCircleArcExpanderStage.js
+++ LinkedCircleArcExpanderStage.js
@@ -74,3 +74,28 @@ class LinkedCircleArcExpanderStage extends CanvasStage {
         stage.handleTap()
     }
 }
+
+class CAEState {
+    constructor() {
+        this.scale = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+
+    update(cb) {
+        this.scale += CAE_updateScale(this.scale, this.dir, CAE_arcs, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb(this.prevScale)
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
